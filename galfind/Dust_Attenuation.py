Updated dust examples, and corrected Calzetti k(λ) calculation to extrapolate results for λ outside usual range.

galfind/Dust_Attenuation.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import astropy.units as u
 from scipy.optimize import curve_fit
+from scipy.interpolate import interp1d
 from abc import ABC, abstractmethod
 
 class Dust_Attenuation(ABC):
@@ -43,7 +44,7 @@ class Dust_Attenuation(ABC):
         """
         pass
 
-    def attenuation(self, wavs, E_BminusV):
+    def attenuate(self, wavs, E_BminusV):
         """ Function to calculate the amount of dust attenuation at wavelength(s) λ, A(λ) = m(λ) - m(λ,0)
 
         Args:
@@ -87,7 +88,7 @@ class Dust_Attenuation(ABC):
         return slope[0]
 
     @property
-    def near_IR_slope(self, NIR_range = [0.9, 5] * u.um, resolution = 0.1 * u.um):
+    def near_IR_slope(self, NIR_range = [0.9, 5.] * u.um, resolution = 0.1 * u.um):
         """ Property giving the near-IR slope of the dust attenuation curve, β_NIR
         The equation concerning this slope is as follows:
         A(λ)/A(V) = (λ / 5500 Angstrom)^-β_NIR, or equivalently
@@ -146,17 +147,20 @@ class Calzetti00(Dust_Attenuation):
         else:
             k = np.zeros(len(wavs.value))
             wavs = np.array([wav.to(u.um).value for wav in wavs])
-        
-        # extrapolate bluewards of 0.12 microns
-        # mask_1 = 
-        # k[mask_1] = 
+
+        mask_1 = (wavs <= 0.12)
         mask_2 = ((wavs > 0.12) & (wavs <= 0.63))
-        k[mask_2] = 2.659 * (-2.156 + (1.509 / (wavs[mask_2])) - (0.198 / (wavs[mask_2] ** 2)) + (0.011 / (wavs[mask_2] ** 3))) + self.R_V
         mask_3 = ((wavs > 0.63) & (wavs <= 2.2))
+        mask_4 = (wavs > 2.2)
+
+        k[mask_2] = 2.659 * (-2.156 + (1.509 / (wavs[mask_2])) - (0.198 / (wavs[mask_2] ** 2)) + (0.011 / (wavs[mask_2] ** 3))) + self.R_V
         k[mask_3] = 2.659 * (-1.857 + 1.040 / wavs[mask_3]) + self.R_V
-        # extrapolate redwards of 2.2 microns
-        # mask_4 = 
-        # k[mask_4] = 
+        if len(wavs[mask_2]) > 1 and len(wavs[mask_1]) > 0:
+            # extrapolate bluewards of 0.12 microns
+            k[mask_1] = interp1d(wavs[mask_2], k[mask_2], fill_value = "extrapolate")(wavs[mask_1])
+        if len(wavs[mask_3]) > 1 and len(wavs[mask_4]) > 0:
+            # extrapolate redwards of 2.2 microns
+            k[mask_4] = interp1d(wavs[mask_3], k[mask_3], fill_value = "extrapolate")(wavs[mask_4])
         return k
 
 

galfind/SED.py
@@ -441,17 +441,29 @@ class Mock_SED_obs(SED_obs):
             colour = -2.5 * np.log10(self.mock_photometry[bands[0]] / self.mock_photometry[bands[1]])
             # save colour in Mock_SED object
             if "colours" in self.__dict__:
-                self.colours = {**self.colours, **{colour_name: colour}}
+                self.colours = {**self.colours, **{colour_name: colour.value}}
             else:
-                self.colours = {colour_name: colour}
-            return colour
+                self.colours = {colour_name: colour.value}
+            return colour.value
         else:
             galfind_logger.critical("self.mock_photometry does not exist! Please first create photometry from SED template!")
     
     def add_DLA(self, DLA_obj):
+        self.colours = {} # reset colours
+        self.DLA = DLA_obj
         self.wavs = funcs.convert_wav_units(self.wavs, u.AA)
         self.mags = funcs.convert_mag_units(self.wavs, self.mags, u.Jy)
         self.mags *= DLA_obj.transmission(self.wavs / (1 + self.z))
+
+    def add_dust_attenuation(self, dust_attenuation, E_BminusV):
+        self.colours = {} # reset colours
+        self.dust_attenuation = dust_attenuation
+        self.E_BminusV = E_BminusV
+        self.wavs = funcs.convert_wav_units(self.wavs, u.AA)
+        self.mags = funcs.convert_mag_units(self.wavs, self.mags, u.Jy)
+        # attenuate flux in Jy (named self.mags)
+        print(dust_attenuation)
+        self.mags *= 10 ** (-0.4 * dust_attenuation.attenuate(self.wavs, E_BminusV))
     
     def add_emission_lines(self, line_diagnostics):
         pass
@@ -516,7 +528,7 @@ class Mock_SED_rest_template_set(Mock_SED_template_set):
         return cls(mock_SED_rest_arr)
     
     @classmethod
-    def load_bpass_in_templates(cls, metallicity = "z020", imf = "imf135_300", model_type = "bin", alpha_enhancement = "a+06", log_ages = "all", bpass_version = 2.3, m_UV_norm = 26.):
+    def load_bpass_in_templates(cls, metallicity = "z020", imf = "imf135_300", model_type = "bin", alpha_enhancement = "a+06", log_ages = np.linspace(6., 9., int(np.round(3 / 0.1)) + 1), bpass_version = 2.3, m_UV_norm = 26.):
         meta = {"metallicity": metallicity, "imf": imf, "model_type": model_type, "alpha_enhancement": alpha_enhancement, "bpass_version": bpass_version}
         bpass_Lsun = 3.848e26 * u.J / u.s
         bpass_dir = f"/raid/scratch/data/BPASS/BPASS_v{str(bpass_version)}_release/bpass_v{str(bpass_version)}.{alpha_enhancement}"