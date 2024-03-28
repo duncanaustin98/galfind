82880c089934db237902376f0fa728f8700a0380
Duncan Austin <duncan.austin@postgrad.manchester.ac.uk>
2024-03-28 12:17:30 +0000

    Started Multiple_Data and Multiple_Catalogue classes in prep for COSMOS-Web.

---
+++ galfind/Multiple_Catalogue.py
@@ -0,0 +1,20 @@
+# Multiple_Catalogue.py
+
+class Multiple_Catalogue:
+
+    def __init__(self, cat_arr):
+        self.cat_arr = cat_arr
+
+    @classmethod
+    def from_pipeline(cls):
+        cat_arr = []
+        return cls(cat_arr)
+
+    def calc_UVLF(self):
+        pass
+
+    def calc_GSMF(self):
+        pass
+
+    def plot(self, x_name, y_name, colour_by, save = False, show = False):
+        pass
