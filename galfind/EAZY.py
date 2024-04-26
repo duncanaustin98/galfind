Catalogue load in works up to PDF loading. Normalization fixed upon PDF intialization.

galfind/EAZY.py
@@ -346,9 +346,9 @@ class EAZY(SED_code):
             assert len(IDs) == len(PDF_paths), galfind_logger.critical(f"len(IDs) = {len(IDs)} != len(data_paths) = {len(PDF_paths)}!")
             # ensure all data_paths are the same and are of .h5 type
             assert all(PDF_path == PDF_paths[0] for PDF_path in PDF_paths), galfind_logger.critical("All data_paths must be the same!")
-            assert PDF_paths[0][:-3] == ".h5", galfind_logger.critical(f"{PDF_paths[0]} must have .h5 file extension")
+            assert PDF_paths[0][-3:] == ".h5", galfind_logger.critical(f"{PDF_paths[0]} must have .h5 file extension")
             # open .h5 file
-            hf = hdf5.File(PDF_paths[0], "r")
+            hf = h5py.File(PDF_paths[0], "r")
             hf_z = np.array(hf["z"])
             # extract redshift PDF for each ID
             redshift_pdfs = [Redshift_PDF(hf_z, np.array(hf[f"ID={str(int(ID))}"]["p(z)"])) \

galfind/PDF.py
@@ -10,9 +10,8 @@ class PDF:
     def __init__(self, property_name, x, p_x):
         self.property_name = property_name
         self.x = x
-        self.p_x = p_x
-        # ensure it is normalized
-        assert(np.trapz(p_x, x) == 1)
+        # normalize to np.trapz(p_x, x) == 1
+        self.p_x = p_x / np.trapz(p_x, x)
 
     @classmethod
     def from_1D_arr(cls):