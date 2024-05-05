Fixed bandpass average flux calculations and minor ABmag SED plot autoscaling issues.

galfind/Filter.py
@@ -5,6 +5,7 @@ import astropy.units as u
 from astroquery.svo_fps import SvoFps
 
 from . import galfind_logger
+from . import useful_funcs_austind as funcs
 
 class Filter:
 
@@ -61,9 +62,12 @@ class Filter:
         output_prop["WavelengthLower50"] = output_prop["WavelengthCen"] - output_prop["FWHM"] / 2.
         return cls(facility, instrument, band, wav, trans, output_prop)
     
-    def plot_filter_profile(self, ax, band, from_SVO = True, color = "black"):
-        ax.fill_between(self.wav, 0., self.trans, color = color, alpha = 0.6)
-        ax.plot(self.wav, self.trans, color = "black", lw = 2) #cmap[np.where(self.bands == band)])
-        mid_wav = np.median(self.wav[self.trans > 1e-3])
-        ax.text(self.WavelengthCen, np.max(self.trans) + 0.03, band, ha = "center", fontsize = 8)
-        ax.grid(False)
+    #def crop_wav_range(self, lower_throughput, upper_throughput):
+    #    self.wavs = self.wavs[self.trans > 1e-1]
+    
+    def plot_filter_profile(self, ax, wav_units = u.um, from_SVO = True, color = "black"):
+        wavs = funcs.convert_wav_units(self.wav, wav_units).value
+        ax.fill_between(wavs, 0., self.trans, color = color, alpha = 0.6)
+        ax.plot(wavs, self.trans, color = "black", lw = 2) #cmap[np.where(self.bands == band)])
+        ax.text(funcs.convert_wav_units(self.WavelengthCen, wav_units).value, \
+            np.max(self.trans) + 0.03, self.band_name, ha = "center", fontsize = 8)

galfind/Instrument.py
@@ -246,19 +246,18 @@ class Instrument:
     #     for band in self:
     #         band.load_band_filter_profile(band, from_SVO = from_SVO)
 
-    def plot_filter_profiles(self, ax, plot_bands = [], from_SVO = True, cmap_name = "Spectral_r", show = True, save = False):
-        cmap = sns.color_palette(cmap_name, len(plot_bands))
-        for i, band in enumerate(plot_bands):
-            if not band in self.band_names:
-                raise(Exception(f"{band} not in {self.name}!"))
-            else:
-                band.plot_filter_profile(ax, band, from_SVO = from_SVO, color = cmap[i])
-        ax.set_title(f"{self.name} filters")
-        ax.set_xlabel(r"$\lambda_{\mathrm{obs}} / \mathrm{\AA}$")
-        ax.set_ylabel("Transmission")
+    def plot_filter_profiles(self, ax, wav_units = u.um, from_SVO = True, \
+            cmap_name = "Spectral_r", annotate = True, show = True, save = False):
+        cmap = sns.color_palette(cmap_name, len(self))
+        for i, band in enumerate(self):
+            band.plot_filter_profile(ax, from_SVO = from_SVO, color = cmap[i])
+        if annotate:
+            ax.set_title(f"{self.name} filters")
+            ax.set_xlabel(r"$\lambda_{\mathrm{obs}}$ / " + funcs.unit_labels_dict[wav_units])
+            ax.set_ylabel("Transmission")
+            ax.set_ylim(0., np.max([trans for trans in band.trans for band in self]) + 0.1)
         if save:
-            print("Figure not saved!")
-            pass
+            plt.savefig(f"{self.name}_filter_profiles.png")
         if show:
             plt.show()
 

galfind/Photometry.py
@@ -101,7 +101,7 @@ class Photometry:
 
     def plot_phot(self, ax, wav_units = u.AA, mag_units = u.Jy, plot_errs = {"x": True, "y": True}, \
             annotate = True, uplim_sigma = 2., auto_scale = True, \
-            errorbar_kwargs = {"ls": "", "marker": "o", "ms": 6., "zorder": 100., "path_effects": [pe.withStroke(linewidth = 3, foreground = "white")]}, \
+            errorbar_kwargs = {"ls": "", "marker": "o", "ms": 4., "zorder": 100., "path_effects": [pe.withStroke(linewidth = 2., foreground = "white")]}, \
             filled = True, colour = "black", label = "Photometry", return_extra = False):
 
         wavs_to_plot = funcs.convert_wav_units(self.wav, wav_units).value
@@ -112,7 +112,7 @@ class Photometry:
         else:
             # work out optimal size of error bar in terms of sigma
             if mag_units == u.ABmag:
-                uplim_sigma_arrow = 0.5
+                uplim_sigma_arrow = 1.5
             else:
                 uplim_sigma_arrow = {"power density/spectral flux density wav": 1.5, \
                     "ABmag/spectral flux density": 1.5, "spectral flux density": 1.5}[str(u.get_physical_type(mag_units))]
@@ -182,18 +182,18 @@ class Photometry:
             # auto-scale the y-axis based on plotting units
             if mag_units == u.ABmag:
                 if plot_errs["y"]:
-                    lower_ylim = np.nanmax(mags_to_plot - yerr[0]) + 0.25
-                    upper_ylim = np.nanmin(mags_to_plot + yerr[1]) - 0.75
+                    lower_ylim = np.nanmax(mags_to_plot + yerr[1]) + 0.25
+                    upper_ylim = np.nanmin(mags_to_plot - yerr[0]) - 0.75
                 else:
-                    lower_ylim = np.nanmax(mags_to_plot) + 0.25
-                    upper_ylim = np.nanmin(mags_to_plot) - 0.75
+                    lower_ylim = np.max(mags_to_plot) + 0.25
+                    upper_ylim = np.min(mags_to_plot) - 0.75
             else: # auto-scale flux units
                 if plot_errs["y"]:
                     lower_ylim = np.nanmin(mags_to_plot - yerr[0]) - 0.15
                     upper_ylim = np.nanmax(mags_to_plot + yerr[1]) + 0.35
                 else:
-                    lower_ylim = np.nanmin(mags_to_plot) - 0.15
-                    upper_ylim = np.nanmax(mags_to_plot) + 0.35
+                    lower_ylim = np.min(mags_to_plot) - 0.15
+                    upper_ylim = np.max(mags_to_plot) + 0.35
             ax.set_ylim(lower_ylim, upper_ylim)
 
         if mag_units == u.ABmag: