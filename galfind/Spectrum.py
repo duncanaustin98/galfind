e70e8a6dd5b60bb805bd7997a93681d5da71ea03
Duncan Austin <duncan.austin@postgrad.manchester.ac.uk>
2024-05-10 17:15:53 +0100

    Started spectrum class

---
+++ galfind/Spectrum.py
@@ -0,0 +1,18 @@
+# Spectrum.py
+
+from typing import NoReturn, Union
+import astropy.units as u
+
+class _Instrument
+
+class Spectrum:
+
+    def __init__(self, 
+            wavs: u.Quantity, 
+            fluxes: Union[u.Quantity, u.Magnitude], 
+            resolution: u.Quantity, 
+            facility: str
+            ) -> NoReturn:
+        pass
+
+
