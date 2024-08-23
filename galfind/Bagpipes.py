fix: SED_result units now load into properties (and property_PDFs) appropriately - but not property_errs fix: Units now correct and tested for Bagpipes (#47) feat: Extended source corrections load and create new PDFs at the original save path with _ext_src_corr added to property_name

galfind/Bagpipes.py
@@ -48,8 +48,8 @@ class Bagpipes(SED_code):
     }
     gal_property_unit_dict = {
         "z": u.dimensionless_unscaled,
-        "stellar_mass": u.solMass, 
-        "formed_mass": u.solMass, 
+        "stellar_mass": "dex(solMass)", 
+        "formed_mass": "dex(solMass)", 
         "dust:Av": u.ABmag,
         "beta_C94": u.dimensionless_unscaled,
         "m_UV": u.ABmag, 
@@ -61,7 +61,6 @@ class Bagpipes(SED_code):
         "ssfr": u.yr ** -1, 
         "ssfr_10myr": u.yr ** -1
     }
-    requires_unlogging = ["stellar_mass", "formed_mass", "ssfr", "ssfr_10myr"] #, "nebular:logU"]
     galaxy_property_dict = {**{gal_property if "redshift" not in gal_property else "z": f"{gal_property}_50" for gal_property in galaxy_properties}, **{"chi_sq": "chisq_phot"}}
     galaxy_property_errs_dict = {gal_property if "redshift" not in gal_property else "z": [f"{gal_property}_16", f"{gal_property}_84"] for gal_property in galaxy_properties}
     available_templates = ["BC03", "BPASS"]
@@ -224,16 +223,16 @@ class Bagpipes(SED_code):
         pdf_arrs = [np.array(Table.read(path, format = "ascii.fast_no_header")["col1"]) \
             if type(path) != type(None) else None for path in \
             tqdm(PDF_paths, desc = f"Loading {gal_property} PDFs", total = len(PDF_paths))]
-        if gal_property in Bagpipes.requires_unlogging:
-            pdf_arrs = [10 ** pdf if type(pdf) != type(None) else None for pdf in pdf_arrs]
         if gal_property == "z":
-            pdfs = [Redshift_PDF.from_1D_arr(pdf * Bagpipes.gal_property_unit_dict[gal_property], \
+            pdfs = [Redshift_PDF.from_1D_arr(pdf * u.Unit(Bagpipes.gal_property_unit_dict[gal_property]), \
                 SED_fit_params, timed = timed) if type(pdf) != type(None) else None \
                 for pdf in tqdm(pdf_arrs, desc = f"Constructing {gal_property} PDFs", total = len(pdf_arrs))]
         else:
-            pdfs = [SED_fit_PDF.from_1D_arr(gal_property, pdf * Bagpipes.gal_property_unit_dict[gal_property], \
+            pdfs = [SED_fit_PDF.from_1D_arr(gal_property, pdf * u.Unit(Bagpipes.gal_property_unit_dict[gal_property]), \
                 SED_fit_params, timed = timed) if type(pdf) != type(None) else None \
                 for pdf in tqdm(pdf_arrs, desc = f"Constructing {gal_property} PDFs", total = len(pdf_arrs))]
+        # add save path to PDF
+        pdfs = [pdf.add_save_path(path) for path, pdf in zip(PDF_paths, pdfs)]
         return pdfs
     
     def load_pipes_fit_obj(self):
@@ -242,7 +241,7 @@ class Bagpipes(SED_code):
     @staticmethod
     def get_out_paths(cat, SED_fit_params, IDs, load_properties = \
             ["stellar_mass", "formed_mass", "dust:Av", \
-            "beta_C94", "m_UV", "M_UV"]): # , "Halpha_EWrest", "xi_ion_caseB"
+            "beta_C94", "m_UV", "M_UV", "sfr", "sfr_10myr"]): # , "Halpha_EWrest", "xi_ion_caseB"
         pipes_name = Bagpipes.label_from_SED_fit_params(SED_fit_params)
         in_path = None
         out_path = f"{config['Bagpipes']['BAGPIPES_DIR']}/cats/{cat.survey}/{pipes_name.replace('Bagpipes_', '')}.fits"

galfind/PDF.py
@@ -56,7 +56,7 @@ class PDF:
         
     def __add__(self, other: Union["PDF", int, float, u.Quantity, u.Magnitude, u.Dex], \
             name_ext: Union[str, None] = None, add_kwargs: dict = {}, save: bool = False):
-        
+
         if type(other) in [int, float, u.Quantity, u.Magnitude, u.Dex]:
             # multiply input array by other
             if hasattr(self, "input_arr"):
@@ -67,8 +67,8 @@ class PDF:
             new_kwargs = {**self.kwargs, **add_kwargs}
         else: # PDF
             # for extending length of PDF
-            assert type(self) == type(other)
-            assert self.property_name == other.property_name
+            assert type(self) == type(other), galfind_logger.critical(f"{type(self)=}!={type(other)=}")
+            assert self.property_name == other.property_name, galfind_logger.critical(f"{self.property_name=}!={other.property_name=}")
             # update kwargs
             new_kwargs = {**self.kwargs, **other.kwargs, **add_kwargs}
             if hasattr(self, "input_arr") and hasattr(other, "input_arr"):
@@ -78,13 +78,21 @@ class PDF:
 
         if type(name_ext) == type(None):
             new_property_name = self.property_name
-        else: # new_property_name == str
-            assert type(new_property_name) in [str]
+        else: # type(name_ext) == str
+            assert type(name_ext) in [str], galfind_logger.critical(f"{name_ext=} with {type(name_ext)=} not in [str]!")
             if name_ext[0] != "_":
                 name_ext = f"_{name_ext}"
             new_property_name = f"{self.property_name}{name_ext}"
-
-        PDF_obj = globals()[self.__class__.__name__].from_1D_arr(new_property_name, new_input_arr, kwargs = new_kwargs)
+        
+        if self.__class__.__name__ == "PDF":
+            PDF_obj = globals()[self.__class__.__name__].from_1D_arr(new_property_name, new_input_arr, kwargs = new_kwargs)
+        elif self.__class__.__name__ == "SED_fit_PDF":
+            PDF_obj = globals()[self.__class__.__name__].from_1D_arr(new_property_name, new_input_arr, self.SED_fit_params, kwargs = new_kwargs)
+        elif self.__class__.__name__ == "Redshift_PDF":
+            PDF_obj = globals()[self.__class__.__name__].from_1D_arr(new_input_arr, self.SED_fit_params, kwargs = new_kwargs)
+        else:
+            galfind_logger.critical(f"{self.__class__.__name__=} not in [PDF, SED_fit_PDF, Redshift_PDF]!")
+            breakpoint()
         # if chosen to save and it has a different name, save the PDF
         if save and hasattr(self, "save_path") and new_property_name != self.property_name:
             PDF_obj.save_PDF(self.save_path.replace(self.property_name, new_property_name))
@@ -107,13 +115,21 @@ class PDF:
         
         if type(name_ext) == type(None):
             new_property_name = self.property_name
-        else: # new_property_name == str
-            assert type(new_property_name) in [str]
+        else: # type(name_ext) == str
+            assert type(name_ext) in [str], galfind_logger.critical(f"{name_ext=} with {type(name_ext)=} not in [str]!")
             if name_ext[0] != "_":
                 name_ext = f"_{name_ext}"
             new_property_name = f"{self.property_name}{name_ext}"
         
-        PDF_obj = globals()[self.__class__.__name__].from_1D_arr(new_property_name, new_input_arr, kwargs = new_kwargs)
+        if self.__class__.__name__ == "PDF":
+            PDF_obj = globals()[self.__class__.__name__].from_1D_arr(new_property_name, new_input_arr, kwargs = new_kwargs)
+        elif self.__class__.__name__ == "SED_fit_PDF":
+            PDF_obj = globals()[self.__class__.__name__].from_1D_arr(new_property_name, new_input_arr, self.SED_fit_params, kwargs = new_kwargs)
+        elif self.__class__.__name__ == "Redshift_PDF":
+            PDF_obj = globals()[self.__class__.__name__].from_1D_arr(new_input_arr, self.SED_fit_params, kwargs = new_kwargs)
+        else:
+            galfind_logger.critical(f"{self.__class__.__name__=} not in [PDF, SED_fit_PDF, Redshift_PDF]!")
+            breakpoint()
         # if chosen to save and it has a different name, save the PDF
         if save and hasattr(self, "save_path") and new_property_name != self.property_name:
             PDF_obj.save_PDF(self.save_path.replace(self.property_name, new_property_name))
@@ -233,7 +249,7 @@ class PDF:
         updated_sample = update_func(sample, **kwargs) #[update_func(val, **kwargs) for val in sample]
         return self.__class__.from_1D_arr(new_property_name, updated_sample, {**self.kwargs, **PDF_kwargs})
     
-    def save_PDF(self, save_path: str, size: int = 10_000) -> None:
+    def save_PDF(self, save_path: str, size: int = 10_000, fmt: str = ".ecsv") -> None:
         if hasattr(self, "input_arr"):
             save_arr = self.input_arr
         else:
@@ -242,9 +258,22 @@ class PDF:
             "l1_err": np.round(self.errs.value[0], 3), "u1_err": np.round(self.errs.value[1], 3)}}
         save_tab = Table({self.property_name: save_arr.value})
         save_tab.meta = meta
+        split_save_path = save_path.split(".")
+        if f".{split_save_path[-1]}" != fmt:
+            if len(split_save_path) == 1:
+                save_path = f"{save_path}{fmt}"
+            elif len(split_save_path) == 2:
+                save_path = f"{save_path[:-(len(split_save_path) + 1)]}{fmt}"
+            else:
+                galfind_logger.warning(f"{save_path=} with format=.{split_save_path[-1]} != {fmt=} and {len(split_save_path)=} not in [1, 2]!")
+        funcs.make_dirs(save_path)
         save_tab.write(save_path, overwrite = True)
         funcs.change_file_permissions(save_path)
         self.save_path = save_path
+
+    def add_save_path(self, path): # -> self
+        self.save_path = path
+        return self
 
     def plot(self, ax, annotate: bool = True, annotate_peak_loc: bool = False, colour: str = "black") -> None:
         

galfind/Photometry_obs.py
@@ -106,15 +106,13 @@ class Photometry_obs(Photometry):
                 if band_name in self.FLUX_AUTO.keys())
             # calculate extended source corrections in each band
             ext_src_corrs = {band_name: (self.FLUX_AUTO[band_name] / (self.flux_Jy[i] * \
-                funcs.mag_to_flux_ratio(-aper_corrs[band_name]))).to(u.dimensionless_unscaled) \
-                for i, band_name in enumerate(self.instrument.band_names) \
-                if band_name in self.FLUX_AUTO.keys()}
+                funcs.mag_to_flux_ratio(-aper_corrs[band_name]))).to(u.dimensionless_unscaled).unmasked \
+                for i, band_name in enumerate(self.instrument.band_names) if band_name in self.FLUX_AUTO.keys()}
             # load these into self
             self.load_property(ext_src_corrs, "ext_src_corrs")
     
     def make_ext_src_corrs(self, gal_property: str, origin: Union[str, dict], \
             ext_src_band: Union[str, list, np.array] = "F444W") -> None:
-        breakpoint()
         # determine correct SED fitting results to use
         if type(origin) in [dict]:
             origin_key = origin["code"].label_from_SED_fit_params(origin)
@@ -144,35 +142,35 @@ class Photometry_obs(Photometry):
             else:
                 orig_property = properties[gal_property]
                 orig_property_PDF = property_PDFs[gal_property]
-                gal_property_unit = orig_property.unit
-                assert orig_property_PDF.x.unit == gal_property_unit
+                assert orig_property_PDF.x.unit == orig_property.unit
                 # errors may not necessarily have the same unit
                 ext_src_corr = self.ext_src_corrs[ext_src_band]
                 PDF_add_kwargs = {"ext_src_band": ext_src_band}
-                if type(gal_property_unit) in [u.Magnitude]:
-                    correction = funcs.flux_to_mag_ratio(ext_src_corr.value) * gal_property_unit
+                if type(orig_property) in [u.Magnitude]:
+                    correction = funcs.flux_to_mag_ratio(ext_src_corr.value) * u.mag # units are incorrect
                     updated_property = orig_property + correction
+                    PDF_add_kwargs = {**PDF_add_kwargs, **{"ext_src_corr": correction}}
                     updated_property_PDF = orig_property_PDF.__add__(correction, \
                         name_ext = funcs.ext_src_label, add_kwargs = PDF_add_kwargs, save = True)
-                elif type(gal_property_unit) in [u.Dex]:
+                elif type(orig_property) in [u.Dex]:
                     correction = u.Dex(np.log10(ext_src_corr.value))
                     updated_property = orig_property + correction
+                    PDF_add_kwargs = {**PDF_add_kwargs, **{"ext_src_corr": correction}}
                     updated_property_PDF = orig_property_PDF.__add__(correction, \
                         name_ext = funcs.ext_src_label, add_kwargs = PDF_add_kwargs, save = True)
-                elif type(gal_property_unit) in [u.Quantity]:
+                elif type(orig_property) in [u.Quantity]:
                     updated_property = orig_property * ext_src_corr
+                    PDF_add_kwargs = {**PDF_add_kwargs, **{"ext_src_corr": ext_src_corr}}
                     updated_property_PDF = orig_property_PDF.__mul__(ext_src_corr, \
                         name_ext = funcs.ext_src_label, add_kwargs = PDF_add_kwargs, save = True)
                 else:
-                    galfind_logger.warning(f"{gal_property_unit=} for {gal_property=} at {origin=} not in [Magnitude, Dex, Quantity]")
+                    galfind_logger.warning(f"{gal_property}={orig_property} from {origin=} with {type(orig_property)=} not in [Magnitude, Dex, Quantity]")
                 # update properties and property_PDFs (assume property_errs remain unaffected)
-                properties[gal_property] = updated_property
-                property_PDFs[gal_property] = updated_property_PDF
-                setattr(data_obj, "properties", properties)
-                setattr(data_obj, "property_PDFs", property_PDFs)
-                # save rest_property attributes outside of dict as well
-                if rest_property:
-                    setattr(data_obj, gal_property, updated_property)
+                data_obj.properties[updated_property_PDF.property_name] = updated_property
+                data_obj.property_PDFs[updated_property_PDF.property_name] = updated_property_PDF
+                # save non rest_property attributes outside of dict as well
+                if not rest_property:
+                    setattr(data_obj, updated_property_PDF.property_name, updated_property)
 
     def make_all_ext_src_corrs(self, ext_src_band: Union[str, list, np.array] = "F444W"):
         # extract previously calculated galaxy properties and their origins

galfind/SED_result.py
@@ -168,8 +168,10 @@ class Catalogue_SED_results:
             for key, val in SED_fit_params["code"].galaxy_property_dict.items()} for SED_fit_params in SED_fit_params_arr]
         cat_properties = [{gal_property: list(fits_cat[label]) \
             for gal_property, label in labels.items()} for labels in labels_arr]
-        cat_properties = [[{key: value[i] for key, value in SED_fitting_properties.items()} \
-            for SED_fitting_properties in cat_properties] for i in range(len(fits_cat))] # may include invalid values where SED fitting not performed
+        cat_properties = [[{key: value[i] * u.Unit(SED_fit_params["code"].gal_property_unit_dict[key]) \
+            if key in SED_fit_params["code"].gal_property_unit_dict.keys() else value[i] * u.dimensionless_unscaled \
+            for key, value in SED_fitting_properties.items()} for SED_fit_params, SED_fitting_properties \
+            in zip(SED_fit_params_arr, cat_properties)] for i in range(len(fits_cat))] # may include invalid values where SED fitting not performed
         
         # convert cat_property_errs from array of len(SED_fit_params_arr), with each element a dict of galaxy properties for the entire catalogue with values of arrays of len(fits_cat)
         # to array of len(fits_cat), with each element an array of len(SED_fit_params_arr) containing a dict of properties for a single galaxy
@@ -243,8 +245,9 @@ class Catalogue_SED_results:
     
     @classmethod
     def from_SED_result_inputs(cls, SED_fit_params_arr: Union[list, np.array], \
-            phot_arr: Union[list, np.array], cat_properties, cat_property_errs, \
-            cat_property_PDFs: Union[np.array, None], cat_SEDs: Union[np.array, None]):
+            phot_arr: Union[list, np.array], cat_properties: Union[list, np.array], \
+            cat_property_errs: Union[list, np.array], cat_property_PDFs: Union[np.array, None], \
+            cat_SEDs: Union[np.array, None]):
         
         # if not loaded, construct appropriately shaped None arrays
         if type(cat_property_PDFs) == type(None):