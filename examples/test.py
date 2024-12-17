fix: Code now breaks if selection in crops not performed feat: Number_density_function Vmax calculation fixed feat: Made extsrc correction more flexible when calculating rest frame properties todo: Check why Driver+10 cosmic variance errors are smaller than they should be

examples/test.py
@@ -1,5 +1,7 @@
 import astropy.units as u
 import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy
 from tqdm import tqdm
 from galfind import Filter, Catalogue, Catalogue_Creator, Data, EAZY, LePhare, Bagpipes
 from galfind import Colour_Selector, Unmasked_Instrument_Selector, EPOCHS_Selector
@@ -58,34 +60,6 @@ def test_selection():
 
 def test_pipes():
     
-    JOF_cat = Catalogue.pipeline(
-        survey,
-        version,
-        instrument_names = instrument_names, 
-        version_to_dir_dict = morgan_version_to_dir,
-        aper_diams = aper_diams,
-        forced_phot_band = forced_phot_band,
-        min_flux_pc_err = min_flux_pc_err,
-        crops = {"SELECTION": EPOCHS_Selector(allow_lowz=True). \
-            _get_selection_name(aper_diams[0], \
-            EAZY({"templates": "fsps_larson", "lowz_zmax": None}).label)}
-    )
-
-    #JOF_cat.load_sextractor_Re()
-    # {"templates": "fsps_larson", "lowz_zmax": 4.0}, {"templates": "fsps_larson", "lowz_zmax": 6.0}, 
-
-    SED_fit_params_arr = [{"templates": "fsps_larson", "lowz_zmax": None}]
-    for SED_fit_params in SED_fit_params_arr:
-        EAZY_fitter = EAZY(SED_fit_params)
-        EAZY_fitter(JOF_cat, aper_diams[0], load_PDFs = False, load_SEDs = False, update = True)
-
-    # EPOCHS_JOF_cat = EPOCHS_Selector()(JOF_cat, aper_diams[0], EAZY_fitter, return_copy = True)
-
-    pipes_SED_fit_params = {"fix_z": EAZY_fitter.label, "fesc": None}
-    pipes_fitter = Bagpipes(pipes_SED_fit_params)
-    pipes_fitter(JOF_cat, aper_diams[0], save_PDFs = False, load_SEDs = False, load_PDFs = False, overwrite = False)
-
-def test_UVLF():
     # {"templates": "fsps_larson", "lowz_zmax": 4.0}, {"templates": "fsps_larson", "lowz_zmax": 6.0}, 
     SED_fit_params_arr = [{"templates": "fsps_larson", "lowz_zmax": None}]
 
@@ -99,24 +73,297 @@ def test_UVLF():
         min_flux_pc_err = min_flux_pc_err,
         crops = EPOCHS_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), allow_lowz=True)
     )
+
+    #JOF_cat.load_sextractor_Re()
+
+    for SED_fit_params in SED_fit_params_arr:
+        EAZY_fitter = EAZY(SED_fit_params)
+        EAZY_fitter(JOF_cat, aper_diams[0], load_PDFs = False, load_SEDs = False, update = True)
+
+    # EPOCHS_JOF_cat = EPOCHS_Selector()(JOF_cat, aper_diams[0], EAZY_fitter, return_copy = True)
+
+    pipes_SED_fit_params = {"fix_z": EAZY_fitter.label, "fesc": None}
+    pipes_fitter = Bagpipes(pipes_SED_fit_params)
+    pipes_fitter(JOF_cat, aper_diams[0], save_PDFs = False, load_SEDs = False, load_PDFs = False, overwrite = False)
+
+def test_UVLF():
+    from galfind import Redwards_Lya_Detect_Selector, Sextractor_Bands_Radius_Selector, Xi_Ion_Calculator, UV_Beta_Calculator, Rest_Frame_Property_Limit_Selector
+
+    # {"templates": "fsps_larson", "lowz_zmax": 4.0}, {"templates": "fsps_larson", "lowz_zmax": 6.0}, 
+    SED_fit_params_arr = [{"templates": "fsps_larson", "lowz_zmax": None}]
+
+    test_selection_criteria = [
+        Redwards_Lya_Detect_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), SNR_lims = [5.0], widebands_only = True),
+        Unmasked_Instrument_Selector("NIRCam"),
+    ]
+    test_selection_criteria.extend([
+        Sextractor_Bands_Radius_Selector( \
+        band_names = ["F277W", "F356W", "F444W"], \
+        gtr_or_less = "gtr", lim = 45. * u.marcsec)
+    ])
+    xi_ion_calc = Xi_Ion_Calculator(aper_diams[0], EAZY(SED_fit_params_arr[-1]), ext_src_corrs = None)
+    low_xi_ion = deepcopy(test_selection_criteria)
+    low_xi_ion.extend([Rest_Frame_Property_Limit_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), xi_ion_calc, 25.2 * u.Unit("dex(Hz/erg)"), "less")])
+    high_xi_ion = deepcopy(test_selection_criteria)
+    high_xi_ion.extend([Rest_Frame_Property_Limit_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), xi_ion_calc, 25.2 * u.Unit("dex(Hz/erg)"), "gtr")])
     
-    # JOF_cat.load_sextractor_Re()
+    for crops in [
+        test_selection_criteria,
+        #high_xi_ion,
+        #low_xi_ion,
+        #test_selection_criteria,
+        #EPOCHS_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), allow_lowz=False), 
+        #EPOCHS_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), allow_lowz=True)
+    ]:
+        JOF_cat = Catalogue.pipeline(
+            survey,
+            version,
+            instrument_names = instrument_names, 
+            version_to_dir_dict = morgan_version_to_dir,
+            aper_diams = aper_diams,
+            forced_phot_band = forced_phot_band,
+            min_flux_pc_err = min_flux_pc_err,
+            #crops = crops, #EPOCHS_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), allow_lowz=False)
+        )
+        #JOF_cat.load_sextractor_Re()
+        #JOF_cat.load_sextractor_ext_src_corrs()
+
+        for SED_fit_params in SED_fit_params_arr:
+            EAZY_fitter = EAZY(SED_fit_params)
+            EAZY_fitter(JOF_cat, aper_diams[0], load_PDFs = True, load_SEDs = True, update = True)
+            
+        xi_ion_calc(JOF_cat, n_chains = 10_000, output = False, n_jobs = 1)
+        Rest_Frame_Property_Limit_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), xi_ion_calc, 25.2 * u.Unit("dex(Hz/erg)"), "less")(JOF_cat)
+        breakpoint()
+
+        #Redwards_Lya_Detect_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), SNR_lims = [5.0], widebands_only = True)(JOF_cat)
+        #Rest_Frame_Property_Limit_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), beta_calc, -2.2 * u.dimensionless_unscaled, "gtr")(JOF_cat)
+        #Rest_Frame_Property_Limit_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), beta_calc, -2.2 * u.dimensionless_unscaled, "less")(JOF_cat)   
+
+        # epochs_selector = EPOCHS_Selector(aper_diams[0], EAZY_fitter, allow_lowz = False, unmasked_instruments = "NIRCam")
+        # epochs_selected_cat = epochs_selector(JOF_cat, return_copy = True)
+        # epochs_selector_lowz = EPOCHS_Selector(aper_diams[0], EAZY_fitter, allow_lowz = True, unmasked_instruments = "NIRCam")
+        # epochs_selected_cat_lowz = epochs_selector_lowz(JOF_cat, return_copy = True)
+
+        from galfind import MUV_Calculator
+        MUV_calculator = MUV_Calculator(aper_diams[0], EAZY_fitter.label)
+        MUV_calculator(JOF_cat, n_chains = 10_000, output = False, n_jobs = 1)
+
+        # from galfind import Xi_Ion_Calculator, M99
+        # for beta_dust_conv in [None]: #, M99]: #, Reddy18(C00(), 100 * u.Myr), Reddy18(C00(), 300 * u.Myr)]:
+        #     for fesc_conv in [None]: #, "Chisholm22"]: # None, 0.1, 0.2, 0.5,
+        #         calculator = Xi_Ion_Calculator(aper_diams[0], EAZY_fitter.label, beta_dust_conv = beta_dust_conv, fesc_conv = fesc_conv)
+        #         calculator(JOF_cat, n_chains = 10_000, output = False, n_jobs = 1)
+
+        from galfind import Number_Density_Function
+        this_work_plot_kwargs = {
+            "mfc": "gray",
+            "marker": "D",
+            "ms": 8.0,
+            "mew": 2.0,
+            "mec": "black",
+            "ecolor": "black",
+            "elinewidth": 2.0,
+        }
+
+        MUV_arr = np.arange(-21.25, -17.25, 0.5) * u.ABmag
+        z5_obs_author_years = {
+            "Bouwens+21": {}
+        }
+        z6_obs_author_years = {
+            "Bowler+15": {},
+            "Bouwens+21": {},
+        }
+        z7_obs_author_years = {
+            "Bouwens+21": {},
+        }
+        z8_obs_author_years = {
+            "Stefanon+19": {},
+            "Bouwens+21": {},
+            "Adams+24": {},
+        }
+        z9_obs_author_years = {
+            "Stefanon+19": {},
+            "Bouwens+21": {},
+            "Adams+24": {},
+        }
+        z10_obs_author_years = {
+            "Bouwens+21": {},
+            "Adams+24": {},
+        }
+        z12_obs_author_years = {
+            "Bouwens+23": {},
+            "Adams+24": {},
+        }
+        obs_author_years = [
+            z5_obs_author_years,
+            z6_obs_author_years,
+            z7_obs_author_years,
+            z8_obs_author_years,
+            z9_obs_author_years,
+            z10_obs_author_years,
+            z12_obs_author_years
+        ]
+        z_bins = [
+            [4.5, 5.5],
+            [5.5, 6.5],
+            [6.5, 7.5],
+            [7.5, 8.5],
+            [8.5, 9.5],
+            [9.5, 11.5],
+            [11.5, 13.5]
+        ]
+        for z_bin, obs_author_years in zip(z_bins, obs_author_years):
+            UVLF = Number_Density_Function.from_single_cat(
+                JOF_cat,
+                MUV_calculator,
+                MUV_arr,
+                z_bin,
+                aper_diam = aper_diams[0],
+                SED_fit_code = EAZY_fitter,
+                x_origin = "phot_rest",
+            )
+            if UVLF is not None:
+                UVLF.plot(
+                    obs_author_years=obs_author_years,
+                    plot_kwargs=this_work_plot_kwargs,
+                )
+
+    # xi_ion_arr = np.arange(23.5, 26.5, 0.5) * u.Unit("dex(Hz/erg)")
+    # xi_ion_func_z5 = Number_Density_Function.from_single_cat(
+    #     JOF_cat,
+    #     calculator,
+    #     xi_ion_arr,
+    #     [4.5, 5.5],
+    #     aper_diam = aper_diams[0],
+    #     SED_fit_code = EAZY_fitter,
+    #     x_origin = "phot_rest",
+    # )
+
+    # xi_ion_func_z5.plot(
+    #     #x_lims=calculator.name,
+    #     #obs_author_years=z5_obs_author_years,
+    #     plot_kwargs=this_work_plot_kwargs,
+    # )
+
+    # xi_ion_func_z6 = Number_Density_Function.from_single_cat(
+    #     JOF_cat,
+    #     calculator,
+    #     xi_ion_arr,
+    #     [5.5, 6.5],
+    #     aper_diam = aper_diams[0],
+    #     SED_fit_code = EAZY_fitter,
+    #     x_origin = "phot_rest",
+    # )
+    # xi_ion_func_z6.plot(
+    #     #x_lims=calculator.name,
+    #     #obs_author_years=z9_author_years,
+    #     plot_kwargs=this_work_plot_kwargs,
+    # )
+
+    # UVLF_z8 = Number_Density_Function.from_single_cat(
+    #     JOF_cat,
+    #     calculator,
+    #     np.arange(-21.25, -17.25, 0.5) * u.ABmag,
+    #     [7.5, 8.5],
+    #     aper_diam = aper_diams[0],
+    #     SED_fit_code = EAZY_fitter,
+    #     x_origin = "phot_rest",
+    # )
+    # UVLF_z8.plot(
+    #     #x_lims=calculator.name,
+    #     #obs_author_years=z9_author_years,
+    #     plot_kwargs=this_work_plot_kwargs,
+    # )
+
+    # UVLF_z9 = Number_Density_Function.from_single_cat(
+    #     JOF_cat,
+    #     calculator,
+    #     np.arange(-21.25, -17.25, 0.5) * u.ABmag,
+    #     [8.5, 9.5],
+    #     aper_diam = aper_diams[0],
+    #     SED_fit_code = EAZY_fitter,
+    #     x_origin = "phot_rest",
+    # )
+    # # z9_author_years = {
+    # #     "Bouwens+21": {"z_ref": 9.0, "plot_kwargs": {}},
+    # #     "Harikane+22": {"z_ref": 9.0, "plot_kwargs": {}},
+    # #     "Finkelstein+23": {"z_ref": 9.0, "plot_kwargs": {}},
+    # #     "Leung+23": {"z_ref": 9.0, "plot_kwargs": {}},
+    # #     "Perez-Gonzalez+23": {"z_ref": 9.0, "plot_kwargs": {}},
+    # #     "Adams+24": {"z_ref": 9.0, "plot_kwargs": {}},
+    # # } # also Finkelstein+22, but wide z bin used
+    # UVLF_z9.plot(
+    #     x_lims=calculator.name,
+    #     #obs_author_years=z9_author_years,
+    #     plot_kwargs=this_work_plot_kwargs,
+    # )
+
+def split_UVLF_by_beta():
+    from galfind import Redwards_Lya_Detect_Selector, Sextractor_Bands_Radius_Selector, UV_Beta_Calculator, Rest_Frame_Property_Limit_Selector
+
+    SED_fit_params_arr = [{"templates": "fsps_larson", "lowz_zmax": None}]
+
+    test_selection_criteria = [
+        Redwards_Lya_Detect_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), SNR_lims = [5.0], widebands_only = True),
+        Unmasked_Instrument_Selector("NIRCam"),
+    ]
+    test_selection_criteria.extend([
+        Sextractor_Bands_Radius_Selector( \
+        band_names = ["F277W", "F356W", "F444W"], \
+        gtr_or_less = "gtr", lim = 45. * u.marcsec)
+    ])
+    beta_calc = UV_Beta_Calculator(aper_diams[0], EAZY(SED_fit_params_arr[-1]))
+    blue_beta = deepcopy(test_selection_criteria)
+    blue_beta.extend([Rest_Frame_Property_Limit_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), beta_calc, -2.2 * u.dimensionless_unscaled, "less")])
+    red_beta = deepcopy(test_selection_criteria)
+    red_beta.extend([Rest_Frame_Property_Limit_Selector(aper_diams[0], EAZY(SED_fit_params_arr[-1]), beta_calc, -2.2 * u.dimensionless_unscaled, "gtr")])
+    
+    JOF_cat = Catalogue.pipeline(
+        survey,
+        version,
+        instrument_names = instrument_names, 
+        version_to_dir_dict = morgan_version_to_dir,
+        aper_diams = aper_diams,
+        forced_phot_band = forced_phot_band,
+        min_flux_pc_err = min_flux_pc_err,
+        crops = test_selection_criteria,
+    )
+    JOF_cat_blue_beta = Catalogue.pipeline(
+        survey,
+        version,
+        instrument_names = instrument_names, 
+        version_to_dir_dict = morgan_version_to_dir,
+        aper_diams = aper_diams,
+        forced_phot_band = forced_phot_band,
+        min_flux_pc_err = min_flux_pc_err,
+        crops = blue_beta,
+    )
+    JOF_cat_red_beta = Catalogue.pipeline(
+        survey,
+        version,
+        instrument_names = instrument_names, 
+        version_to_dir_dict = morgan_version_to_dir,
+        aper_diams = aper_diams,
+        forced_phot_band = forced_phot_band,
+        min_flux_pc_err = min_flux_pc_err,
+        crops = red_beta,
+    )
 
     for SED_fit_params in SED_fit_params_arr:
         EAZY_fitter = EAZY(SED_fit_params)
         EAZY_fitter(JOF_cat, aper_diams[0], load_PDFs = True, load_SEDs = True, update = True)
-
-    # epochs_selector = EPOCHS_Selector(aper_diams[0], EAZY_fitter, allow_lowz = False, unmasked_instruments = "NIRCam")
-    # epochs_selected_cat = epochs_selector(JOF_cat, return_copy = True)
-    # epochs_selector_lowz = EPOCHS_Selector(aper_diams[0], EAZY_fitter, allow_lowz = True, unmasked_instruments = "NIRCam")
-    # epochs_selected_cat_lowz = epochs_selector_lowz(JOF_cat, return_copy = True)
+        EAZY_fitter(JOF_cat_blue_beta, aper_diams[0], load_PDFs = True, load_SEDs = True, update = True)
+        EAZY_fitter(JOF_cat_red_beta, aper_diams[0], load_PDFs = True, load_SEDs = True, update = True)
 
     from galfind import MUV_Calculator
     MUV_calculator = MUV_Calculator(aper_diams[0], EAZY_fitter.label)
     MUV_calculator(JOF_cat, n_chains = 10_000, output = False, n_jobs = 1)
+    MUV_calculator(JOF_cat_blue_beta, n_chains = 10_000, output = False, n_jobs = 1)
+    MUV_calculator(JOF_cat_red_beta, n_chains = 10_000, output = False, n_jobs = 1)
 
     from galfind import Number_Density_Function
-    this_work_plot_kwargs = {
+    cat_plot_kwargs = {
         "mfc": "gray",
         "marker": "D",
         "ms": 8.0,
@@ -125,28 +372,64 @@ def test_UVLF():
         "ecolor": "black",
         "elinewidth": 2.0,
     }
-    UVLF_z9 = Number_Density_Function.from_single_cat(
-        JOF_cat,
-        MUV_calculator,
-        np.arange(-21.25, -17.25, 0.5) * u.ABmag,
-        [8.5, 9.5],
-        aper_diam = aper_diams[0],
-        SED_fit_code = EAZY_fitter,
-        x_origin = "phot_rest",
-    )
-    z9_author_years = {
-        "Bouwens+21": {"z_ref": 9.0, "plot_kwargs": {}},
-        "Harikane+22": {"z_ref": 9.0, "plot_kwargs": {}},
-        "Finkelstein+23": {"z_ref": 9.0, "plot_kwargs": {}},
-        "Leung+23": {"z_ref": 9.0, "plot_kwargs": {}},
-        "Perez-Gonzalez+23": {"z_ref": 9.0, "plot_kwargs": {}},
-        "Adams+24": {"z_ref": 9.0, "plot_kwargs": {}},
-    } # also Finkelstein+22, but wide z bin used
-    UVLF_z9.plot(
-        x_lims=MUV_calculator.name,
-        author_year_dict=z9_author_years,
-        plot_kwargs=this_work_plot_kwargs,
-    )
+    blue_plot_kwargs = {
+        "mfc": "lightblue",
+        "marker": "o",
+        "ms": 8.0,
+        "mew": 2.0,
+        "mec": "blue",
+        "ecolor": "blue",
+        "elinewidth": 2.0,
+    }
+    red_plot_kwargs = {
+        "mfc": "lightcoral",
+        "marker": "o",
+        "ms": 8.0,
+        "mew": 2.0,
+        "mec": "red",
+        "ecolor": "red",
+        "elinewidth": 2.0,
+    }
+    MUV_arr = np.arange(-21.25, -17.25, 0.5) * u.ABmag
+    z5_obs_author_years = {
+        "Bouwens+21": {}
+    }
+    z6_obs_author_years = {
+        "Bowler+15": {},
+        "Bouwens+21": {},
+    }
+    obs_author_years = [
+        z5_obs_author_years,
+        z6_obs_author_years,
+    ]
+    z_bins = [
+        [4.5, 5.5],
+        [5.5, 6.5],
+    ]
+    cats = [JOF_cat, JOF_cat_blue_beta, JOF_cat_red_beta]
+    plot_kwargs_arr = [cat_plot_kwargs, blue_plot_kwargs, red_plot_kwargs]
+    for z_bin, obs_author_years in zip(z_bins, obs_author_years):
+        fig, ax = plt.subplots()
+        for i, (cat, plot_kwargs) in enumerate(zip(cats, plot_kwargs_arr)):
+            UVLF = Number_Density_Function.from_single_cat(
+                cat,
+                MUV_calculator,
+                MUV_arr,
+                z_bin,
+                aper_diam = aper_diams[0],
+                SED_fit_code = EAZY_fitter,
+                x_origin = "phot_rest",
+            )
+            if UVLF is not None:
+                UVLF.plot(
+                    fig,
+                    ax,
+                    obs_author_years=obs_author_years if i == len(cats) - 1 else {},
+                    plot_kwargs=plot_kwargs,
+                    save = True if i == len(cats) - 1 else False,
+                    save_name = f"{JOF_cat.crop_name.split('/')[-1]}+{z_bin[0]}<z<{z_bin[1]}_beta_split_-2.2" if i == len(cats) - 1 else None,
+                    title = r"5$\sigma$ detected, $\beta$ split"
+                )
 
 
 def main():
@@ -184,6 +467,7 @@ if __name__ == "__main__":
     #main()
     #test_selection()
     test_UVLF()
+    #split_UVLF_by_beta()
     #test_pipes()
     #check_multinest()
 

galfind/Catalogue_Base.py
@@ -70,8 +70,8 @@ class Catalogue_Base:
         output_str += funcs.band_sep
         output_str += f"CAT PATH = {self.cat_path}\n"
         # access table header to display what has been run for this catalogue
-        cat = self.cat_creator.open_cat(self.cat_path, "ID")
-        output_str += f"TOTAL GALAXIES = {len(cat)}\n"
+        tab = self.cat_creator.open_cat(self.cat_path, "ID")
+        output_str += f"TOTAL GALAXIES = {len(tab)}\n"
         output_str += f"RA RANGE = {self.ra_range}\n"
         output_str += f"DEC RANGE = {self.dec_range}\n"
         output_str += funcs.band_sep
@@ -127,7 +127,7 @@ class Catalogue_Base:
 
     def __getitem__(self, index: Any) -> Optional[Union[Galaxy, List[Galaxy]]]:
         if len(self) == 0:
-            return self
+            raise IndexError("No galaxies in catalogue!")
         if isinstance(index, int):
             return self.gals[index]
         elif isinstance(index, (list, np.ndarray)):
@@ -165,7 +165,7 @@ class Catalogue_Base:
         elif isinstance(index, tuple(Selector.__subclasses__())):
             # run selection if not already done
             if not all(index.name in gal.selection_flags for gal in self):
-                index(self)
+                [index(gal, return_copy = False) for gal in self]
             keep_arr = [gal.selection_flags[index.name] for gal in self]
             return list(np.array(self.gals)[np.array(keep_arr)])
     
@@ -179,7 +179,8 @@ class Catalogue_Base:
 
     # only acts on attributes that don't already exist in Catalogue
     def __getattr__(
-        self, property_name: str #, origin: Union[str, dict] = "gal"
+        self: Self,
+        property_name: str #, origin: Union[str, dict] = "gal"
     ) -> np.ndarray:
         # get attributes from stored galaxy objects
         # if property_name in self.__dict__.keys():
@@ -200,8 +201,7 @@ class Catalogue_Base:
                 assert all(
                     attr.unit == attr_arr[0].unit for attr in attr_arr
                 )
-                attr_arr = [attr.value for attr in attr_arr] \
-                    * u.Unit(attr_arr[0].unit)
+                attr_arr = [attr.value for attr in attr_arr] * u.Unit(attr_arr[0].unit)
             return attr_arr
         else:
             raise AttributeError
@@ -217,12 +217,11 @@ class Catalogue_Base:
                 else:
                     setattr(gal, name, value)
 
-    # not needed!
     def __setitem__(self, index, gal):
         self.gals[index] = gal
 
     def __deepcopy__(self, memo):
-        galfind_logger.info(f"deepcopy({self.__class__.__name__})")
+        galfind_logger.debug(f"deepcopy({self.__class__.__name__})")
         cls = self.__class__
         result = cls.__new__(cls)
         memo[id(self)] = result

galfind/Number_Density_Function.py
@@ -9,7 +9,7 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import Table
-from typing import NoReturn, Optional, TYPE_CHECKING
+from typing import NoReturn, Optional, Dict, Any, TYPE_CHECKING
 if TYPE_CHECKING:
     from . import Catalogue, Multiple_Catalogue, Rest_Frame_Property_Calculator
 try:
@@ -43,7 +43,7 @@ class Base_Number_Density_Function:
     ) -> Self:
         if x_name in ["M1500", "M_UV", "MUV"]:
             x_name = "UVLF"
-        if type(z_ref) in [str, int]:
+        if isinstance(z_ref, (str, int)):
             z_ref = float(z_ref)
         x_name_config_key = f"{x_name}_LIT_DIR"
         ecsv_data_path = f"{config['NumberDensityFunctions'][x_name_config_key]}/z={z_ref:.1f}/{author_year}.ecsv"
@@ -72,6 +72,9 @@ class Base_Number_Density_Function:
 
         flags_property_name_conv = {
             "M1500": "LUV",
+            "M1500_[1250,3000]AA": "LUV",
+            "M1500_[1250,3000]AA_extsrc": "LUV",
+            "M1500_[1250,3000]AA_extsrc_UV<10": "LUV",
             "M_UV": "LUV",
             "stellar_mass": "Mstar",
         }
@@ -83,7 +86,7 @@ class Base_Number_Density_Function:
         z_ref = (z_bin[0] + z_bin[1]) / 2.0
         for pos, path in enumerate(datasets):
             ds = distribution_functions.read(path, verbose=False)
-            if ds.name == author_year:
+            if all(string in ds.name for string in author_year.split("+")):
                 # choose closest redshift to bin centre
                 z = None
                 deltaz = 100
@@ -94,9 +97,10 @@ class Base_Number_Density_Function:
                         if delta_z_i < deltaz:
                             deltaz = delta_z_i
                             z = float(z_i)
-                if type(z) == type(None):
+                if z is None:
                     galfind_logger.warning(
-                        f"No available redshift for {author_year=} in {z_bin=}! Available redshifts are {ds.redshifts}"
+                        f"No available redshift for {author_year=} in {z_bin=}!" + \
+                        f" Available redshifts are {ds.redshifts}"
                     )
                     return None
                 else:
@@ -119,8 +123,9 @@ class Base_Number_Density_Function:
                     err_high = 10 ** (log10phi + high) - 10**log10phi
                     err_low = (10**log10phi) - 10 ** (log10phi - low)
                     phi_err = np.array([err_low, err_high])
-
-                    if x_name in ["M1500", "M_UV"]:
+                    
+                    if x_name in [key for (key, val) in \
+                            flags_property_name_conv.items() if val == "LUV"]:
                         x = ds.M[z]
                     else:
                         x = ds.log10X[z]
@@ -128,6 +133,7 @@ class Base_Number_Density_Function:
                     return cls(
                         x_name, x, z, 10**log10phi, phi_err, author_year
                     )
+        galfind_logger.info(f"No {author_year=} in {obs_or_models} for {x_name=}")
         return None  # if no author_year in flags_data
 
     def get_z_bin_name(self) -> str:
@@ -137,22 +143,29 @@ class Base_Number_Density_Function:
         self,
         fig=None,
         ax=None,
-        log: bool = False,
+        log_x: bool = False,
+        log_y: bool = False,
         annotate: bool = False,
         save: bool = False,
         show: bool = False,
         plot_kwargs: dict = {},
         legend_kwargs: dict = {},
-        x_lims: Union[list, np.array, str, None] = "default",
-        save_name: Union[str, None] = None,
-    ) -> None:
+        x_lims: Optional[Union[list, np.array, str]] = "default",
+        title: Optional[str] = None,
+        save_name: Optional[str] = None,
+    ) -> NoReturn:
         if all(i is None for i in [fig, ax]):
             fig, ax = plt.subplots()
 
         # don't plot empty bins
-        x_mid_bins = np.array(
-            [_x for _x, _y in zip(self.x_mid_bins, self.phi) if _y != 0.0]
-        )
+        if isinstance(self.x_mid_bins, (u.Quantity, u.Magnitude, u.Dex)):
+            x_mid_bins = np.array(
+                [_x for _x, _y in zip(self.x_mid_bins.value, self.phi) if _y != 0.0]
+            )
+        else:
+            x_mid_bins = np.array(
+                [_x for _x, _y in zip(self.x_mid_bins, self.phi) if _y != 0.0]
+            )
         phi = np.array([_y for _y in self.phi if _y != 0.0])
         phi_errs_cv = np.array(
             [
@@ -168,7 +181,7 @@ class Base_Number_Density_Function:
                 ],
             ]
         )
-        if log:
+        if log_y:
             y = np.log10(phi)
             y_errs = np.array(
                 [
@@ -200,14 +213,23 @@ class Base_Number_Density_Function:
 
         if annotate:
             y_label = r"$\Phi$ / N dex$^{-1}$Mpc$^{-3}$"
-            if log:
+            if log_x:
+                x_label = r"$\log_{10}($" + self.x_name + ")"
+                ax.set_xscale("log")
+            else:
+                x_label = self.x_name
+            if log_y:
                 y_label = r"$\log_{10}($" + y_label + ")"
             else:
                 ax.set_yscale("log")
             ax.set_xlabel(self.x_name)
             ax.set_ylabel(y_label)
+            if title is not None:
+                ax.set_title(title)
             if x_lims is not None:
                 if isinstance(x_lims, str):
+                    if x_lims == "default":
+                        x_lims = self.x_name
                     ax.set_xlim(*funcs.default_lims[x_lims])
                 else:
                     assert len(x_lims) == 2
@@ -226,14 +248,16 @@ class Base_Number_Density_Function:
         if save:
             if self.__class__.__name__ != "Number_Density_Function":
                 assert save_name is not None
-                plot_path = f"{config['NumberDensityFunctions']['NUMBER_DENSITY_FUNC_DIR']}/Plots/Literature/{save_name}"
+                plot_path = config['NumberDensityFunctions']['NUMBER_DENSITY_FUNC_DIR'] + \
+                    f"/Plots/Literature/{save_name}"
             else:
                 plot_path = self.get_plot_path()
-            if os.access(plot_path, os.W_OK):
-                funcs.make_dirs(plot_path)
-            else:
-                galfind_logger.warning(f"Cannot write to {plot_path}!")
-            plt.savefig(plot_path)
+                if save_name is not None:
+                    plot_path = "/".join(plot_path.split("/")[:-1]) + f"/{save_name}.png"
+            funcs.make_dirs(plot_path)
+            plt.savefig(plot_path, bbox_inches="tight")
+            funcs.change_file_permissions(plot_path)
+            galfind_logger.info(f"Saved plot to {plot_path}")
         if show:
             plt.show()
 
@@ -241,17 +265,19 @@ class Base_Number_Density_Function:
 class Number_Density_Function(Base_Number_Density_Function):
     def __init__(
         self,
-        x_name,
+        x_name: str,
         x_bins,
         x_origin,
         z_bin,
-        Ngals,
+        Ngals: int,
         phi,
         phi_errs,
         cv_errs,
         origin_surveys,
+        crop_name: str,
         cv_origin,
     ) -> Self:
+        self.crop_name = crop_name
         self.x_bins = x_bins
         self.x_origin = x_origin
         self.z_bin = z_bin
@@ -295,6 +321,7 @@ class Number_Density_Function(Base_Number_Density_Function):
         x_origin = tab.meta["x_origin"]
         x_name = tab.meta["x_name"]
         origin_surveys = tab.meta["origin_surveys"]
+        crop_name = tab.meta["crop_name"]
         z_bin = tab.meta["z_bin"]
         return cls(
             x_name,
@@ -306,6 +333,7 @@ class Number_Density_Function(Base_Number_Density_Function):
             np.array([phi_l1, phi_u1]),
             cv_errs,
             origin_surveys,
+            crop_name,
             cv_origin,
         )
 
@@ -369,7 +397,7 @@ class Number_Density_Function(Base_Number_Density_Function):
         cv_origin: Union[str, None] = "Driver2010",
         save: bool = True,
         timed: bool = False,
-    ) -> Self:
+    ) -> Optional[Self]:
         # input assertions
         assert len(z_bin) == 2
         assert z_bin[0] < z_bin[1]
@@ -394,24 +422,40 @@ class Number_Density_Function(Base_Number_Density_Function):
             x = [gal.aper_phot[aper_diam].SED_results[SED_fit_code.label].phot_rest.properties[x_calculator.name] for gal in cat]
         else: # x_origin == "SED_result":
             x = [gal.aper_phot[aper_diam].SED_results[SED_fit_code.label].properties[x_calculator.name] for gal in cat]
+        # remove nans
+        x = [x_ for x_ in x if not np.isnan(x_)]
         assert all(x_.unit == x[0].unit for x_ in x)
         x = np.array([x_.value for x_ in x]) * x[0].unit
 
         # determine save_path
         origin_surveys = Number_Density_Function.get_origin_surveys(data_arr)
         save_path = Number_Density_Function.get_save_path(
-            origin_surveys, SED_fit_code.label, z_bin, x_calculator.name
+            origin_surveys,
+            x_origin,
+            x_calculator.name,
+            cat.crop_name,
         )
-
         if not Path(save_path).is_file():
-            # extract z_bin_name
-            z_bin_name = funcs.get_SED_fit_label_aper_diam_z_bin_name(SED_fit_code.label, aper_diam, z_bin)
             # crop catalogue to this redshift bin
             from . import Redshift_Limit_Selector, Redshift_Bin_Selector
             # TODO: Implement Redshift_Limit_Selector in case of np.nan z_bin entry
             z_bin_selector = Redshift_Bin_Selector(aper_diam, SED_fit_code.label, z_bin)
             z_bin_cat = deepcopy(cat).crop(z_bin_selector)
-
+            # ensure every galaxy in this redshift bin has 
+            # the relevant property already calculated
+            if len(z_bin_cat) == 0:
+                galfind_logger.warning(
+                    f"No galaxies in {z_bin=}"
+                )
+                return None
+            elif len([i for i, gal in enumerate(z_bin_cat) if \
+                    np.isnan(gal.aper_phot[aper_diam].SED_results \
+                    [SED_fit_code.label].phot_rest.properties \
+                    [x_calculator.name])]) != 0:
+                galfind_logger.warning(
+                    f">1 nan in {x_calculator.name} for {z_bin=}, skipping!"
+                )
+                return None
             # create x_bins from x_bin_edges (must include start and end values here too)
             x_bins = [
                 [x_bin_edges[i].value, x_bin_edges[i + 1].value] * x_bin_edges.unit
@@ -432,12 +476,15 @@ class Number_Density_Function(Base_Number_Density_Function):
             phi_l1 = np.zeros(len(x_bins))
             phi_u1 = np.zeros(len(x_bins))
             cv_errs = np.zeros(len(x_bins))
-            phi_errs_cv = np.zeros(len(x_bins))
+            #phi_errs_cv = np.zeros(len(x_bins))
             # loop through each mass bin in the given redshift bin
             for i, x_bin in enumerate(x_bins):
                 if len(z_bin_cat) == 0:
                     Ngals[i] = 0
                 else:
+                    # plot histogram
+                    #hist_fig, hist_ax = plt.subplots()
+                    #z_bin_cat.hist(x_calculator, hist_fig, hist_ax)
                     # crop to galaxies in the x bin - not the bootstrapping method
                     from . import Rest_Frame_Property_Limit_Selector, Rest_Frame_Property_Bin_Selector
                     # TODO: Implement Rest_Frame_Property_Limit_Selector in case of np.nan x_bin entry
@@ -446,17 +493,19 @@ class Number_Density_Function(Base_Number_Density_Function):
                     Ngals[i] = len(z_bin_x_bin_cat)
                 # if there are galaxies in the z,x bin
                 if int(Ngals[i]) != 0:
+                    # plot histogram
+                    #z_bin_x_bin_cat.hist(x_calculator, hist_fig, hist_ax)
                     dx = x_bin[1].value - x_bin[0].value
                     # calculate Vmax's
                     V_max = np.zeros(int(Ngals[i])).astype(float)
                     for data in data_arr:
                         V_max += np.array(
                             [
-                                gal.V_max[z_bin_name][data.full_name]
-                                if gal.V_max[z_bin_name][data.full_name]
-                                != -1.0
-                                else 0.0
-                                for gal in z_bin_x_bin_cat
+                                gal.aper_phot[aper_diam].SED_results[SED_fit_code.label]. \
+                                V_max[z_bin_cat.crop_name.split("/")[-1]][data.full_name]
+                                if gal.aper_phot[aper_diam].SED_results[SED_fit_code.label]. \
+                                V_max[z_bin_cat.crop_name.split("/")[-1]][data.full_name] != -1.0
+                                else 0.0 for gal in z_bin_x_bin_cat
                             ]
                         )
                     V_max = np.array(
@@ -490,7 +539,6 @@ class Number_Density_Function(Base_Number_Density_Function):
                         )
                     else:
                         raise NotImplementedError
-
             number_density_func = cls(
                 x_calculator.name,
                 x_bins,
@@ -501,15 +549,14 @@ class Number_Density_Function(Base_Number_Density_Function):
                 np.array([phi_l1, phi_u1]),
                 cv_errs,
                 origin_surveys,
+                z_bin_cat.crop_name,
                 cv_origin,
             )
-
-            if save and not Path(save_path).is_file():
+            if save:
                 number_density_func.save()
-
             return number_density_func
 
-        else:  # load results
+        else: # load results
             return cls.from_ecsv(save_path)
 
     @staticmethod
@@ -521,16 +568,14 @@ class Number_Density_Function(Base_Number_Density_Function):
     def get_save_path(
         origin_surveys: str,
         SED_fit_params_key: str,
-        z_bin: Union[list, np.array],
         x_name: str,
+        crop_name: str,
         ext: str = ".ecsv",
     ) -> str:
         save_path = config['NumberDensityFunctions']['NUMBER_DENSITY_FUNC_DIR'] + \
-            f"/Data/{SED_fit_params_key}/{x_name}/{origin_surveys}" + \
-            f"/{z_bin[0]}<z<{z_bin[1]}{ext}"
-
-        if os.access(save_path, os.W_OK):
-            funcs.make_dirs(save_path)
+            f"/Data/{SED_fit_params_key}/{x_name}/" + \
+            f"{origin_surveys}/{crop_name}{ext}"
+        funcs.make_dirs(save_path)
         return save_path
 
     # cv_origin == "Driver2010"
@@ -548,15 +593,15 @@ class Number_Density_Function(Base_Number_Density_Function):
         )
         return SED_fit_params_key, x_name, origin_surveys, z_bin
 
-    def get_z_bin_name(self) -> str:
-        return f"{self.z_bin[0]:.1f}<z<{self.z_bin[1]:.1f}"
+    # def get_z_bin_name(self) -> str:
+    #     return f"{self.z_bin[0]:.1f}<z<{self.z_bin[1]:.1f}"
 
     def get_plot_path(self) -> str:
         plot_path = self.get_save_path(
             self.origin_surveys,
-            self.x_origin.replace("_REST_PROPERTY", ""),
-            self.z_bin,
+            self.x_origin,
             self.x_name,
+            self.crop_name,
             ext=".png",
         ).replace("/Data/", "/Plots/")
         if os.access(plot_path, os.W_OK):
@@ -569,9 +614,9 @@ class Number_Density_Function(Base_Number_Density_Function):
         if save_path is None:
             save_path = self.get_save_path(
                 self.origin_surveys,
-                self.x_origin.replace("_REST_PROPERTY", ""),
-                self.z_bin,
+                self.x_origin,
                 self.x_name,
+                self.crop_name,
             )
         x_bins_low = np.array([x_bin[0].value for x_bin in self.x_bins])
         x_bins_up = np.array([x_bin[1].value for x_bin in self.x_bins])
@@ -593,6 +638,7 @@ class Number_Density_Function(Base_Number_Density_Function):
             "origin_surveys": self.origin_surveys,
             "z_bin": self.z_bin,
             "cv_origin": self.cv_origin,
+            "crop_name": self.crop_name
         }
         funcs.make_dirs(save_path)
         tab.write(save_path, overwrite=True)
@@ -603,31 +649,39 @@ class Number_Density_Function(Base_Number_Density_Function):
 
     def plot(
         self,
-        fig=None,
-        ax=None,
-        log: bool = False,
+        fig: Optional[plt.Figure] = None,
+        ax: Optional[plt.Axes] = None,
+        log_x: bool = False,
+        log_y: bool = False,
         annotate: bool = True,
         save: bool = True,
         show: bool = False,
-        plot_kwargs: dict = {},
-        legend_kwargs: dict = {},
+        plot_kwargs: Dict[str, Any] = {},
+        legend_kwargs: Dict[str, Any] = {},
         x_lims: Union[list, np.array, str, None] = "default",
-        obs_author_years: dict = {},
-        sim_author_years: dict = {},
-    ) -> None:
-        if all(type(_x) == type(None) for _x in [fig, ax]):
+        title: Optional[str] = None,
+        obs_author_years: Dict[str, Any] = {},
+        sim_author_years: Dict[str, Any] = {},
+        save_name: Optional[str] = None,
+    ) -> NoReturn:
+        if all(_x is None for _x in [fig, ax]):
             fig, ax = plt.subplots()
+
+        if title is None:
+            title = self.crop_name
+
         for author_year, author_year_kwargs in obs_author_years.items():
             author_year_func_from_flags_data = (
                 Base_Number_Density_Function.from_flags_repo(
                     self.x_name, self.z_bin, author_year, "obs"
                 )
             )
-            if type(author_year_func_from_flags_data) != type(None):
+            if author_year_func_from_flags_data is not None:
                 author_year_func_from_flags_data.plot(
                     fig,
                     ax,
-                    log,
+                    log_x,
+                    log_y,
                     annotate=False,
                     save=False,
                     show=False,
@@ -640,28 +694,33 @@ class Number_Density_Function(Base_Number_Density_Function):
                     self.x_name, self.z_bin, author_year, "models"
                 )
             )
-            if type(author_year_func_from_flags_data) != type(None):
+            if author_year_func_from_flags_data is not None:
                 author_year_func_from_flags_data.plot(
                     fig,
                     ax,
-                    log,
+                    log_x,
+                    log_y,
                     annotate=False,
                     save=False,
                     show=False,
                     plot_kwargs=author_year_kwargs,
                     x_lims=None,
                 )
+
         # plot this work
         super().plot(
             fig,
             ax,
-            log,
+            log_x,
+            log_y,
             annotate,
             save,
             show,
             plot_kwargs,
             legend_kwargs,
             x_lims,
+            title,
+            save_name,
         )
 
 
@@ -756,7 +815,7 @@ class Multiple_Number_Density_Function:
                 if i != len(x_bin_edges) - 1
             ]
             # extract z_bin_name
-            assert type(x_origin) == str  # NOT GENERAL!
+            assert isinstance(x_origin, str)
             z_bin_name = f"{x_origin}_{z_bin[0]:.1f}<z<{z_bin[1]:.1f}"
             # calculate Vmax for each galaxy in catalogue within z bin
             # in general call Vmax_multifield

galfind/Photometry.py
@@ -10,12 +10,13 @@ from __future__ import annotations
 
 from copy import deepcopy
 import time
+import matplotlib.pyplot as plt
 from abc import ABC
 import astropy.units as u
 import matplotlib.patheffects as pe
 import numpy as np
 from numpy.typing import NDArray
-from typing import Union, List, Dict, Any, TYPE_CHECKING
+from typing import Union, Optional, List, Dict, Any, TYPE_CHECKING
 if TYPE_CHECKING:
     from . import Instrument
     from astropy.utils.masked import Masked
@@ -205,7 +206,8 @@ class Photometry:
 
     @property
     def wav(self):
-        return self.instrument.band_wavelengths
+        return np.array([filt.WavelengthCen.to(u.AA).value \
+            for filt in self.filterset]) * u.AA
 
     # @classmethod
     # def from_fits_cat(cls, fits_cat_row, instrument, cat_creator):
@@ -263,9 +265,9 @@ class Photometry:
 
     def plot(
         self: Self,
-        ax,
-        wav_units: u.Quantity = u.AA,
-        mag_units: Union[u.Quantity, u.Magnitude] = u.Jy,
+        ax: Optional[plt.Axes] = None,
+        wav_units: u.Unit = u.AA,
+        mag_units: u.Unit = u.Jy,
         plot_errs={"x": True, "y": True},
         annotate=True,
         uplim_sigma=2.0,
@@ -378,17 +380,17 @@ class Photometry:
                 [
                     [
                         funcs.convert_wav_units(
-                            filter.WavelengthCen - filter.WavelengthLower50,
+                            filt.WavelengthCen - filt.WavelengthLower50,
                             wav_units,
                         ).value
-                        for filter in self.instrument
+                        for filt in self.filterset
                     ],
                     [
                         funcs.convert_wav_units(
-                            filter.WavelengthUpper50 - filter.WavelengthCen,
+                            filt.WavelengthUpper50 - filt.WavelengthCen,
                             wav_units,
                         ).value
-                        for filter in self.instrument
+                        for filt in self.filterset
                     ],
                 ]
             )

galfind/Photometry_obs.py
@@ -5,6 +5,7 @@ import time
 import astropy.units as u
 from astropy.utils.masked import Masked
 import matplotlib.patheffects as pe
+import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 from copy import deepcopy
@@ -12,7 +13,7 @@ from photutils.aperture import (
     CircularAperture,
     aperture_photometry,
 )
-from typing import TYPE_CHECKING, Union, List, Dict, NoReturn
+from typing import TYPE_CHECKING, Union, List, Dict, NoReturn, Optional
 if TYPE_CHECKING:
     from . import Multiple_Filter, SED_result, Multiple_Band_Cutout
 try:
@@ -268,204 +269,156 @@ class Photometry_obs(Photometry):
     ) -> None:
         setattr(self, save_name, gal_property)
 
-    def calc_ext_src_corrs(self, aper_corrs: Union[dict, None]) -> None:
-        # FLUX_AUTO must already be loaded
-        if not hasattr(self, "FLUX_AUTO"):
-            galfind_logger.critical(
-                "Could not calculate ext_src_corrs as FLUX_AUTO not loaded in!"
-            )
-            raise NotImplementedError
-        # if not already calculated
-        property_name = "_".join(inspect.stack()[0].function.split("_")[1:])
-        if not hasattr(self, property_name):
-            # calculate aperture corrections if not given
-            if aper_corrs is None:
-                aper_corrs = self.filterset.get_aper_corrs(self.aper_diam)
-                aper_corrs = {
-                    band_name: aper_corr
-                    for band_name, aper_corr in zip(
-                        self.instrument.band_names, aper_corrs
-                    )
-                }
-            # ensure all bands in self.instrument which have FLUX_AUTO's have aperture corrections calculated
-            assert all(
-                band_name in aper_corrs.keys()
-                for band_name in self.instrument.band_names
-                if band_name in self.FLUX_AUTO.keys()
-            )
-            # calculate extended source corrections in each band
-            ext_src_corrs = {
-                band_name: (
-                    self.FLUX_AUTO[band_name]
-                    / (
-                        self.flux[i]
-                        * funcs.mag_to_flux_ratio(-aper_corrs[band_name])
-                    )
-                )
-                .to(u.dimensionless_unscaled)
-                .unmasked
-                for i, band_name in enumerate(self.instrument.band_names)
-                if band_name in self.FLUX_AUTO.keys()
-            }
-            ext_src_corrs = {
-                band_name: ext_src_corr
-                if ext_src_corr.value > 1
-                else 1.0 * u.dimensionless_unscaled
-                for band_name, ext_src_corr in ext_src_corrs.items()
-            }
-            # load these into self
-            self.load_property(ext_src_corrs, property_name)
+    # def make_ext_src_corrs(
+    #     self,
+    #     gal_property: str,
+    #     origin: Union[str, dict],
+    #     ext_src_band: Union[str, list, np.array] = "F444W",
+    # ) -> None:
+    #     # determine correct SED fitting results to use
+    #     if type(origin) in [dict]:
+    #         origin_key = origin["code"].label_from_SED_fit_params(origin)
+    #         rest_property = False
+    #     elif type(origin) in [str]:
+    #         if "_REST_PROPERTY" in origin:
+    #             rest_property = True
+    #         else:
+    #             rest_property = False
+    #         origin_key = origin.replace("_REST_PROPERTY", "")
+    #     else:
+    #         galfind_logger.critical(f"{type(origin)=} not in [str, dict]!")
+    #     # skip if key not available
+    #     if origin_key not in self.SED_results.keys():
+    #         galfind_logger.warning(
+    #             f"Could not compute ext_src_corrs for {gal_property=} as {origin_key=} not in {self.SED_results.keys()=}!"
+    #         )
+    #     else:
+    #         if rest_property:
+    #             data_obj = self.SED_results[origin_key].phot_rest
+    #         else:
+    #             data_obj = self.SED_results[origin_key]
+    #         properties = data_obj.properties
+    #         property_errs = data_obj.property_errs
+    #         property_PDFs = data_obj.property_PDFs
+    #         # skip if galaxy property not in properties + property_errs + property_PDFs dicts
+    #         if any(
+    #             gal_property not in property_dict.keys()
+    #             for property_dict in [properties, property_errs, property_PDFs]
+    #         ):
+    #             galfind_logger.warning(
+    #                 f"{gal_property=},{origin_key=},{rest_property=} not in all of [{properties.keys()=},{property_errs.keys()=},{property_PDFs.keys()=}]!"
+    #             )
+    #         else:
+    #             orig_property = properties[gal_property]
+    #             orig_property_PDF = property_PDFs[gal_property]
+    #             assert orig_property_PDF.x.unit == orig_property.unit
+    #             # errors may not necessarily have the same unit
+    #             ext_src_corr = self.ext_src_corrs[ext_src_band]
+    #             PDF_add_kwargs = {"ext_src_band": ext_src_band}
+    #             if type(orig_property) in [u.Magnitude]:
+    #                 correction = (
+    #                     funcs.flux_to_mag_ratio(ext_src_corr.value) * u.mag
+    #                 )  # units are incorrect
+    #                 updated_property = orig_property + correction
+    #                 PDF_add_kwargs = {
+    #                     **PDF_add_kwargs,
+    #                     **{"ext_src_corr": correction},
+    #                 }
+    #                 updated_property_PDF = orig_property_PDF.__add__(
+    #                     correction,
+    #                     name_ext=funcs.ext_src_label,
+    #                     add_kwargs=PDF_add_kwargs,
+    #                     save=True,
+    #                 )
+    #             elif type(orig_property) in [u.Dex]:
+    #                 correction = u.Dex(np.log10(ext_src_corr.value))
+    #                 updated_property = orig_property + correction
+    #                 PDF_add_kwargs = {
+    #                     **PDF_add_kwargs,
+    #                     **{"ext_src_corr": correction},
+    #                 }
+    #                 updated_property_PDF = orig_property_PDF.__add__(
+    #                     correction,
+    #                     name_ext=funcs.ext_src_label,
+    #                     add_kwargs=PDF_add_kwargs,
+    #                     save=True,
+    #                 )
+    #             elif type(orig_property) in [u.Quantity]:
+    #                 updated_property = orig_property * ext_src_corr
+    #                 PDF_add_kwargs = {
+    #                     **PDF_add_kwargs,
+    #                     **{"ext_src_corr": ext_src_corr},
+    #                 }
+    #                 updated_property_PDF = orig_property_PDF.__mul__(
+    #                     ext_src_corr,
+    #                     name_ext=funcs.ext_src_label,
+    #                     add_kwargs=PDF_add_kwargs,
+    #                     save=True,
+    #                 )
+    #             else:
+    #                 galfind_logger.warning(
+    #                     f"{gal_property}={orig_property} from {origin=} with {type(orig_property)=} not in [Magnitude, Dex, Quantity]"
+    #                 )
+    #             # update properties and property_PDFs (assume property_errs remain unaffected)
+    #             data_obj.properties[updated_property_PDF.property_name] = (
+    #                 updated_property
+    #             )
+    #             data_obj.property_PDFs[updated_property_PDF.property_name] = (
+    #                 updated_property_PDF
+    #             )
+    #             # save non rest_property attributes outside of dict as well
+    #             if not rest_property:
+    #                 setattr(
+    #                     data_obj,
+    #                     updated_property_PDF.property_name,
+    #                     updated_property,
+    #                 )
 
-    def make_ext_src_corrs(
-        self,
-        gal_property: str,
-        origin: Union[str, dict],
-        ext_src_band: Union[str, list, np.array] = "F444W",
-    ) -> None:
-        # determine correct SED fitting results to use
-        if type(origin) in [dict]:
-            origin_key = origin["code"].label_from_SED_fit_params(origin)
-            rest_property = False
-        elif type(origin) in [str]:
-            if "_REST_PROPERTY" in origin:
-                rest_property = True
-            else:
-                rest_property = False
-            origin_key = origin.replace("_REST_PROPERTY", "")
-        else:
-            galfind_logger.critical(f"{type(origin)=} not in [str, dict]!")
-        # skip if key not available
-        if origin_key not in self.SED_results.keys():
-            galfind_logger.warning(
-                f"Could not compute ext_src_corrs for {gal_property=} as {origin_key=} not in {self.SED_results.keys()=}!"
-            )
-        else:
-            if rest_property:
-                data_obj = self.SED_results[origin_key].phot_rest
-            else:
-                data_obj = self.SED_results[origin_key]
-            properties = data_obj.properties
-            property_errs = data_obj.property_errs
-            property_PDFs = data_obj.property_PDFs
-            # skip if galaxy property not in properties + property_errs + property_PDFs dicts
-            if any(
-                gal_property not in property_dict.keys()
-                for property_dict in [properties, property_errs, property_PDFs]
-            ):
-                galfind_logger.warning(
-                    f"{gal_property=},{origin_key=},{rest_property=} not in all of [{properties.keys()=},{property_errs.keys()=},{property_PDFs.keys()=}]!"
-                )
-            else:
-                orig_property = properties[gal_property]
-                orig_property_PDF = property_PDFs[gal_property]
-                assert orig_property_PDF.x.unit == orig_property.unit
-                # errors may not necessarily have the same unit
-                ext_src_corr = self.ext_src_corrs[ext_src_band]
-                PDF_add_kwargs = {"ext_src_band": ext_src_band}
-                if type(orig_property) in [u.Magnitude]:
-                    correction = (
-                        funcs.flux_to_mag_ratio(ext_src_corr.value) * u.mag
-                    )  # units are incorrect
-                    updated_property = orig_property + correction
-                    PDF_add_kwargs = {
-                        **PDF_add_kwargs,
-                        **{"ext_src_corr": correction},
-                    }
-                    updated_property_PDF = orig_property_PDF.__add__(
-                        correction,
-                        name_ext=funcs.ext_src_label,
-                        add_kwargs=PDF_add_kwargs,
-                        save=True,
-                    )
-                elif type(orig_property) in [u.Dex]:
-                    correction = u.Dex(np.log10(ext_src_corr.value))
-                    updated_property = orig_property + correction
-                    PDF_add_kwargs = {
-                        **PDF_add_kwargs,
-                        **{"ext_src_corr": correction},
-                    }
-                    updated_property_PDF = orig_property_PDF.__add__(
-                        correction,
-                        name_ext=funcs.ext_src_label,
-                        add_kwargs=PDF_add_kwargs,
-                        save=True,
-                    )
-                elif type(orig_property) in [u.Quantity]:
-                    updated_property = orig_property * ext_src_corr
-                    PDF_add_kwargs = {
-                        **PDF_add_kwargs,
-                        **{"ext_src_corr": ext_src_corr},
-                    }
-                    updated_property_PDF = orig_property_PDF.__mul__(
-                        ext_src_corr,
-                        name_ext=funcs.ext_src_label,
-                        add_kwargs=PDF_add_kwargs,
-                        save=True,
-                    )
-                else:
-                    galfind_logger.warning(
-                        f"{gal_property}={orig_property} from {origin=} with {type(orig_property)=} not in [Magnitude, Dex, Quantity]"
-                    )
-                # update properties and property_PDFs (assume property_errs remain unaffected)
-                data_obj.properties[updated_property_PDF.property_name] = (
-                    updated_property
-                )
-                data_obj.property_PDFs[updated_property_PDF.property_name] = (
-                    updated_property_PDF
-                )
-                # save non rest_property attributes outside of dict as well
-                if not rest_property:
-                    setattr(
-                        data_obj,
-                        updated_property_PDF.property_name,
-                        updated_property,
-                    )
-
-    def make_all_ext_src_corrs(
-        self, ext_src_band: Union[str, list, np.array] = "F444W"
-    ) -> dict:
-        # extract previously calculated galaxy properties and their origins
-        code_ext_src_property_dict = {
-            key: [
-                gal_property
-                for gal_property in self.SED_results[key]
-                .SED_fit_params["code"]
-                .ext_src_corr_properties
-                if gal_property in self.SED_results[key].properties.keys()
-                and gal_property in self.SED_results[key].property_PDFs.keys()
-            ]
-            for key in self.SED_results.keys()
-        }
-        sed_rest_ext_src_property_dict = {
-            f"{key}_REST_PROPERTY": [
-                gal_property
-                for gal_property in self.SED_results[
-                    key
-                ].phot_rest.properties.keys()
-                if gal_property.split("_")[0] in funcs.ext_src_properties
-                and gal_property
-                in self.SED_results[key].phot_rest.property_PDFs.keys()
-            ]
-            for key in self.SED_results.keys()
-        }
-        ext_src_property_dict = {
-            **code_ext_src_property_dict,
-            **sed_rest_ext_src_property_dict,
-        }
-        # make the extended source corrections
-        [
-            self.make_ext_src_corrs(gal_property, origin)
-            for origin, gal_properties in ext_src_property_dict.items()
-            for gal_property in gal_properties
-        ]
-        # return dict of {origin: [property for property in gal[origin]]}
-        return ext_src_property_dict
+    # def make_all_ext_src_corrs(
+    #     self, ext_src_band: Union[str, list, np.array] = "F444W"
+    # ) -> dict:
+    #     # extract previously calculated galaxy properties and their origins
+    #     code_ext_src_property_dict = {
+    #         key: [
+    #             gal_property
+    #             for gal_property in self.SED_results[key]
+    #             .SED_fit_params["code"]
+    #             .ext_src_corr_properties
+    #             if gal_property in self.SED_results[key].properties.keys()
+    #             and gal_property in self.SED_results[key].property_PDFs.keys()
+    #         ]
+    #         for key in self.SED_results.keys()
+    #     }
+    #     sed_rest_ext_src_property_dict = {
+    #         f"{key}_REST_PROPERTY": [
+    #             gal_property
+    #             for gal_property in self.SED_results[
+    #                 key
+    #             ].phot_rest.properties.keys()
+    #             if gal_property.split("_")[0] in funcs.ext_src_properties
+    #             and gal_property
+    #             in self.SED_results[key].phot_rest.property_PDFs.keys()
+    #         ]
+    #         for key in self.SED_results.keys()
+    #     }
+    #     ext_src_property_dict = {
+    #         **code_ext_src_property_dict,
+    #         **sed_rest_ext_src_property_dict,
+    #     }
+    #     # make the extended source corrections
+    #     [
+    #         self.make_ext_src_corrs(gal_property, origin)
+    #         for origin, gal_properties in ext_src_property_dict.items()
+    #         for gal_property in gal_properties
+    #     ]
+    #     # return dict of {origin: [property for property in gal[origin]]}
+    #     return ext_src_property_dict
 
     def plot(
-        self,
-        ax,
-        wav_units: Union[str, u.Unit] = u.AA,
-        mag_units: Union[str, u.Unit] = u.Jy,
+        self: Self,
+        ax: Optional[plt.Axes] = None,
+        wav_units: u.Unit = u.AA,
+        mag_units: u.Unit = u.Jy,
         plot_errs: dict = {"x": True, "y": True},
         annotate: bool = True,
         uplim_sigma: float = 2.0,
@@ -583,6 +536,31 @@ class Photometry_obs(Photometry):
             ax.legend()
 
         return plot
+    
+    def load_sextractor_ext_src_corrs(
+        self: Self, 
+        aper_corrs: Optional[Dict[str, float]] = None
+    ) -> NoReturn:
+        if aper_corrs is None:
+            [filt.instrument._load_aper_corrs() for filt in self.filterset]
+            aper_corrs = {filt.band_name: filt.instrument. \
+                aper_corrs[filt.band_name][self.aper_diam] for filt in self.filterset}
+        assert all(filt_name in aper_corrs.keys() \
+            for filt_name in self.filterset.band_names)
+
+        ext_src_corrs = {filt_name: (self.sex_FLUX_AUTO[filt_name] \
+            / (self.flux[i] * funcs.mag_to_flux_ratio(-aper_corrs[filt_name]))) \
+            .to(u.dimensionless_unscaled).unmasked for i, filt_name \
+            in enumerate(self.filterset.band_names)}
+        self.ext_src_corrs = {
+            filt_name: ext_src_corr.value
+            if ext_src_corr.value > 1.0 else 1.0
+            for filt_name, ext_src_corr in ext_src_corrs.items()
+        }
+        # propagate ext_src_corrs to SED_results[key].phot_rest
+        for key in self.SED_results.keys():
+            self.SED_results[key].phot_rest. \
+                ext_src_corrs = self.ext_src_corrs
 
     # def load_local_depths(self, sex_cat_row, instrument, aper_diam_index):
     #    self.depths = np.array([sex_cat_row[f"loc_depth_{band}"].T[aper_diam_index] for band in instrument.band_names])