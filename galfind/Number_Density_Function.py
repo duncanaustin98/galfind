fix: Rest_Frame_Properties calculation when there are nan depths towards image edges

galfind/Number_Density_Function.py
@@ -153,7 +153,7 @@ class Base_Number_Density_Function:
         legend_kwargs: dict = {},
         x_lims: Optional[Union[list, np.array, str]] = "default",
         title: Optional[str] = None,
-        save_name: Optional[str] = None,
+        save_path: Optional[str] = None,
     ) -> Tuple[plt.Figure, plt.Axes]:
         if all(i is None for i in [fig, ax]):
             fig_, ax_ = plt.subplots()
@@ -249,18 +249,20 @@ class Base_Number_Density_Function:
             _legend_kwargs = {**legend_kwargs, **default_legend_kwargs}
             ax_.legend(**_legend_kwargs)
         if save:
-            if self.__class__.__name__ != "Number_Density_Function":
-                assert save_name is not None
-                plot_path = config['NumberDensityFunctions']['NUMBER_DENSITY_FUNC_DIR'] + \
-                    f"/Plots/Literature/{save_name}"
-            else:
-                plot_path = self.get_plot_path()
-                if save_name is not None:
-                    plot_path = "/".join(plot_path.split("/")[:-1]) + f"/{save_name}.png"
-            funcs.make_dirs(plot_path)
-            plt.savefig(plot_path, bbox_inches="tight")
-            funcs.change_file_permissions(plot_path)
-            galfind_logger.info(f"Saved plot to {plot_path}")
+            # if self.__class__.__name__ != "Number_Density_Function":
+            #     raise NotImplementedError
+            #     # assert save_path is not None
+            #     # save_path = config['NumberDensityFunctions']['NUMBER_DENSITY_FUNC_DIR'] + \
+            #     #     f"/Plots/Literature/{save_name}"
+            # else:
+            if save_path is None:
+                save_path = self.get_plot_path()
+                # if save_name is not None:
+                #     plot_path = "/".join(plot_path.split("/")[:-1]) + f"/{save_name}.png"
+            funcs.make_dirs(save_path)
+            plt.savefig(save_path, bbox_inches="tight")
+            funcs.change_file_permissions(save_path)
+            galfind_logger.info(f"Saved plot to {save_path}")
         if show:
             plt.show()
         return fig_, ax_
@@ -292,6 +294,7 @@ class Number_Density_Function(Base_Number_Density_Function):
         x_mid_bins = np.array(
             [(x_bin[1].value + x_bin[0].value) / 2.0 for x_bin in x_bins]
         ) * x_bins[0].unit
+        
         z_ref = float((z_bin[1] + z_bin[0]) / 2.0)
         phi_errs_cv = np.array(
             [
@@ -308,12 +311,13 @@ class Number_Density_Function(Base_Number_Density_Function):
         tab = Table.read(save_path)
         x_bins_up = np.array(tab["x_bins_up"])
         x_bins_low = np.array(tab["x_bins_low"])
+        x_unit = tab["x_bins_up"].unit
         x_bins = np.array(
             [
                 [x_bin_low, x_bin_up]
                 for x_bin_low, x_bin_up in zip(x_bins_low, x_bins_up)
             ]
-        )
+        ) * x_unit
         Ngals = np.array(tab["Ngals"])
         phi = np.array(tab["phi"])
         phi_l1 = np.array(tab["phi_l1"])
@@ -661,8 +665,11 @@ class Number_Density_Function(Base_Number_Density_Function):
                 self.x_name,
                 self.crop_name,
             )
-        x_bins_low = np.array([x_bin[0].value for x_bin in self.x_bins])
-        x_bins_up = np.array([x_bin[1].value for x_bin in self.x_bins])
+        assert all(x_bin[0].unit == self.x_bins[0][0].unit for x_bin in self.x_bins)
+        assert all(x_bin[1].unit == self.x_bins[0][1].unit for x_bin in self.x_bins)
+        assert self.x_bins[0][0].unit == self.x_bins[0][1].unit
+        x_bins_low = np.array([x_bin[0].value for x_bin in self.x_bins]) * self.x_bins[0][0].unit
+        x_bins_up = np.array([x_bin[1].value for x_bin in self.x_bins]) * self.x_bins[0][1].unit
         tab = Table(
             {
                 "x_bins_low": x_bins_low,
@@ -705,7 +712,7 @@ class Number_Density_Function(Base_Number_Density_Function):
         title: Optional[str] = None,
         obs_author_years: Dict[str, Any] = {},
         sim_author_years: Dict[str, Any] = {},
-        save_name: Optional[str] = None,
+        save_path: Optional[str] = None,
     ) -> Tuple[plt.Figure, plt.Axes]:
         if all(_x is None for _x in [fig, ax]):
             fig_, ax_ = plt.subplots()
@@ -765,7 +772,7 @@ class Number_Density_Function(Base_Number_Density_Function):
             legend_kwargs,
             x_lims,
             title,
-            save_name,
+            save_path,
         )
         return fig_, ax_
 

galfind/Rest_frame_properties.py
@@ -619,11 +619,7 @@ class UV_Beta_Calculator(Rest_Frame_Property_Calculator):
             and filt.WavelengthUpper50 < self.global_kwargs["rest_UV_wav_lims"][1] * (1.0 + phot_rest.z)
         ]
         if len(rest_frame_UV_indices) < 2:
-            rest_frame_UV_indices = None
-            rest_UV_band_wavs = None
-            #beta_fit_obj = None
-            rest_UV_SNRs = None
-            negative_flux_pc = None
+            failure = True
         else:
             rest_UV_band_wavs = np.array(
                 [
@@ -632,16 +628,22 @@ class UV_Beta_Calculator(Rest_Frame_Property_Calculator):
                     in phot_rest.filterset[rest_frame_UV_indices]
                 ]
             ) * u.AA
-            #beta_fit_obj = beta_fit(phot_rest.z, phot_rest.filterset[rest_frame_UV_indices])
             phot_rest_UV = phot_rest[rest_frame_UV_indices]
             rest_UV_SNRs = phot_rest_UV.flux / phot_rest_UV.flux_errs
-            # determine what percentage of scatters fall into the negative flux region
-            negative_flux_pc = (1. - np.prod([1. - norm.cdf(0., loc=mu, scale=std) for mu, std in \
-                zip(phot_rest_UV.flux.value, phot_rest_UV.flux_errs.value)])) * 100.0
-            
+            if any(np.isnan(SNR) for SNR in rest_UV_SNRs):
+                failure = True
+            else:
+                # determine what percentage of scatters fall into the negative flux region
+                negative_flux_pc = (1. - np.prod([1. - norm.cdf(0., loc=mu, scale=std) for mu, std in \
+                    zip(phot_rest_UV.flux.value, phot_rest_UV.flux_errs.value)])) * 100.0
+        if failure:
+            rest_frame_UV_indices = None
+            rest_UV_band_wavs = None
+            rest_UV_SNRs = None
+            negative_flux_pc = None
+        
         return {
             "keep_indices": rest_frame_UV_indices,
-            #"beta_fit": beta_fit_obj, 
             "rest_UV_band_wavs": rest_UV_band_wavs,
             "rest_UV_SNRs": rest_UV_SNRs,
             "negative_flux_pc": negative_flux_pc
@@ -1367,6 +1369,12 @@ class Optical_Continuum_Calculator(Rest_Frame_Property_Calculator):
         # determine the nearest band to the first line
         wavelength = line_diagnostics[self.global_kwargs \
             ["strong_line_names"][0]]["line_wav"] * (1.0 + phot_rest.z)
+        if len(phot_rest.filterset) == 0:
+            return {
+                "emission_band": None,
+                "cont_bands": None,
+                "keep_indices": None,
+            }
         nearest_band = phot_rest.filterset[
             int(np.abs(
                 [
@@ -1408,7 +1416,7 @@ class Optical_Continuum_Calculator(Rest_Frame_Property_Calculator):
                 ):
                     cont_bands.extend([filt])
                     cont_band_indices.extend([i])
-            if len(cont_bands) == 0:
+            if len(cont_bands) == 0 or any(np.isnan(phot_rest.depths[i]) for i in cont_band_indices):
                 cont_bands = None
                 cont_band_indices = None
         return {
@@ -1546,21 +1554,27 @@ class Optical_Line_EW_Calculator(Rest_Frame_Property_Calculator):
             if wavelength > filt.WavelengthLower50
             and wavelength < filt.WavelengthUpper50
         ]
+
         if nearest_band.band_name not in emission_bands:
-            emission_band = None
-            emission_band_index = None
-            emission_band_wavelength = None
-            bandwidth = None
+            failure = True
         else:
             emission_band = nearest_band
             emission_band_index = int(np.where(np.array( \
                 phot_rest.filterset.band_names) == emission_band.band_name)[0][0])
-            emission_band_wavelength = emission_band.WavelengthCen
-            bandwidth = emission_band.WavelengthUpper50 \
-                - emission_band.WavelengthLower50
-            if self.global_kwargs["frame"] == "rest":
-                bandwidth /= (1.0 + phot_rest.z)
-            bandwidth = bandwidth.to(u.AA)
+            if np.isnan(phot_rest.depths[emission_band_index]):
+                failure = True
+            else:
+                emission_band_wavelength = emission_band.WavelengthCen
+                bandwidth = emission_band.WavelengthUpper50 \
+                    - emission_band.WavelengthLower50
+                if self.global_kwargs["frame"] == "rest":
+                    bandwidth /= (1.0 + phot_rest.z)
+                bandwidth = bandwidth.to(u.AA)
+        if failure:
+            emission_band = None
+            emission_band_index = None
+            emission_band_wavelength = None
+            bandwidth = None
         return {
             "emission_band": emission_band,
             "keep_indices": emission_band_index,
@@ -1825,10 +1839,13 @@ class Optical_Line_Flux_Calculator(Rest_Frame_Property_Calculator):
         phot_rest: Photometry_rest
     ) -> Dict[str, Any]:
         emission_band = phot_rest.property_kwargs[self.pre_req_properties[1].name]["band"]
-        band_wav = deepcopy(phot_rest.filterset[emission_band].WavelengthCen)
-        if band_wav is not None:
-            if self.pre_req_properties[1].global_kwargs["frame"] == "rest":
-                band_wav /= (1.0 + phot_rest.z)
+        if np.isnan(phot_rest.depths[emission_band]):
+            band_wav = None
+        else:
+            band_wav = deepcopy(phot_rest.filterset[emission_band].WavelengthCen)
+            if band_wav is not None:
+                if self.pre_req_properties[1].global_kwargs["frame"] == "rest":
+                    band_wav /= (1.0 + phot_rest.z)
         return {"band_wav": band_wav}
 
     def _fail_criteria(
@@ -1981,8 +1998,8 @@ class Xi_Ion_Calculator(Rest_Frame_Property_Calculator):
         resolution: u.Quantity = 1.0 * u.AA,
         fesc_conv: Optional[Union[str, float]] = None,
         logged: bool = True,
-        ext_src_corrs: Optional[str] = "UV",
-        ext_src_uplim: Optional[Union[int, float]] = 10.0,
+        #ext_src_corrs: Optional[str] = "UV",
+        #ext_src_uplim: Optional[Union[int, float]] = 10.0,
     ) -> NoReturn:
         line_lum_calculator = \
             Optical_Line_Luminosity_Calculator(
@@ -2005,8 +2022,8 @@ class Xi_Ion_Calculator(Rest_Frame_Property_Calculator):
             beta_dust_conv,
             top_hat_width,
             resolution,
-            ext_src_corrs,
-            ext_src_uplim,
+            ext_src_corrs = None,
+            ext_src_uplim = None,
         )
         pre_req_properties = [line_lum_calculator, LUV_calculator]
         if fesc_conv is None:
@@ -2044,9 +2061,9 @@ class Xi_Ion_Calculator(Rest_Frame_Property_Calculator):
         line_label = "+".join(self.pre_req_properties[0]. \
             pre_req_properties[0].pre_req_properties[1]. \
             global_kwargs["strong_line_names"])
-        ext_src_label = "_extsrc" if self.pre_req_properties[1]. \
-            pre_req_properties[0].global_kwargs["ext_src_corrs"] else ""
-        label = f"xi_ion_{line_label}{dust_label}_{fesc_label}{ext_src_label}"
+        # ext_src_label = "_extsrc" if self.pre_req_properties[1]. \
+        #     pre_req_properties[0].global_kwargs["ext_src_corrs"] else ""
+        label = f"xi_ion_{line_label}{dust_label}_{fesc_label}" #{ext_src_label}"
         if self.global_kwargs["logged"]:
             label = f"log_{label}"
         return label

galfind/Selector.py
@@ -16,7 +16,7 @@ except ImportError:
     from typing_extensions import Self, Type  # python > 3.7 AND python < 3.11
 
 from . import useful_funcs_austind as funcs
-from . import galfind_logger, config
+from . import galfind_logger, config, wav_lyman_lim
 from . import Galaxy, Catalogue, Instrument, SED_code
 from .Instrument import expected_instr_bands
 
@@ -923,6 +923,72 @@ class Min_Unmasked_Band_Selector(Data_Selector):
         return n_unmasked_bands >= self.kwargs["min_bands"]
 
 
+class Bluewards_LyLim_Non_Detect_Selector(Redshift_Selector):
+
+    def __init__(
+        self: Self,
+        aper_diam: u.Quantity,
+        SED_fit_label: Union[str, SED_code],
+        SNR_lim: float,
+        ignore_bands: Optional[Union[str, List[str]]] = None,
+    ):
+        if isinstance(ignore_bands, str):
+            ignore_bands = [ignore_bands]
+        kwargs = {"SNR_lim": SNR_lim, "ignore_bands": ignore_bands}
+        super().__init__(aper_diam, SED_fit_label, **kwargs)
+
+    @property
+    def _selection_name(self) -> str:
+        selection_name = f"bluewards_LyLim_SNR<{self.kwargs['SNR_lim']:.1f}"
+        if self.kwargs["ignore_bands"] is not None:
+            ignore_str = ",".join(self.kwargs["ignore_bands"])
+            selection_name += f"_no_{ignore_str}"
+        return selection_name
+
+    @property
+    def _include_kwargs(self) -> List[str]:
+        return ["SNR_lim", "ignore_bands"]
+
+    def _assertions(self: Self) -> bool:
+        try:
+            assertions = []
+            assertions.extend([isinstance(self.kwargs["SNR_lim"], (int, float))])
+            if self.kwargs["ignore_bands"] is not None:
+                for band in self.kwargs["ignore_bands"]:
+                    # ensure this band exists
+                    assertions.extend([band in json.loads(config.get("Other", "ALL_BANDS"))])
+            passed = all(assertions)
+        except:
+            passed = False
+        return passed
+        
+    def _selection_criteria(
+        self: Self,
+        gal: Galaxy,
+    ) -> bool:
+        # extract first Lylim non-detect band
+        first_Lylim_non_detect_band = gal.aper_phot[self.aper_diam]. \
+            SED_results[self.SED_fit_label].phot_rest. \
+                get_first_bluewards_band(
+                    wav_lyman_lim * u.AA,
+                    self.kwargs["ignore_bands"],
+                )
+        # if no bands bluewards of Lyman alpha,
+        # select the galaxy by default
+        if first_Lylim_non_detect_band is None:
+            return True
+        # find index of first Lya non-detect band
+        first_Lylim_non_detect_index = np.where(
+            np.array(gal.aper_phot[self.aper_diam].filterset.band_names) \
+            == first_Lylim_non_detect_band)[0][0]
+        SNR_non_detect = gal.aper_phot[self.aper_diam].SNR[: first_Lylim_non_detect_index + 1]
+        mask_non_detect = gal.aper_phot[self.aper_diam].flux.mask[: first_Lylim_non_detect_index + 1]
+        # require the first Lylim non detect band and all bluewards bands 
+        # to be non-detected at < SNR_lim if not masked
+        return all(SNR < self.kwargs["SNR_lim"] or mask for mask, SNR in 
+            zip(mask_non_detect, SNR_non_detect))
+
+
 class Bluewards_Lya_Non_Detect_Selector(Redshift_Selector):
 
     def __init__(
@@ -930,20 +996,37 @@ class Bluewards_Lya_Non_Detect_Selector(Redshift_Selector):
         aper_diam: u.Quantity,
         SED_fit_label: Union[str, SED_code],
         SNR_lim: float,
+        ignore_bands: Optional[Union[str, List[str]]] = None,
     ):
-        kwargs = {"SNR_lim": SNR_lim}
+        if isinstance(ignore_bands, str):
+            ignore_bands = [ignore_bands]
+        kwargs = {"SNR_lim": SNR_lim, "ignore_bands": ignore_bands}
         super().__init__(aper_diam, SED_fit_label, **kwargs)
 
     @property
     def _selection_name(self) -> str:
-        return f"bluewards_Lya_SNR<{self.kwargs['SNR_lim']:.1f}"
+        selection_name = f"bluewards_Lya_SNR<{self.kwargs['SNR_lim']:.1f}"
+        if self.kwargs["ignore_bands"] is not None:
+            ignore_str = ",".join(self.kwargs["ignore_bands"])
+            selection_name += f"_no_{ignore_str}"
+        return selection_name
 
     @property
     def _include_kwargs(self) -> List[str]:
-        return ["SNR_lim"]
+        return ["SNR_lim", "ignore_bands"]
 
     def _assertions(self: Self) -> bool:
-        return isinstance(self.kwargs["SNR_lim"], (int, float))
+        try:
+            assertions = []
+            assertions.extend([isinstance(self.kwargs["SNR_lim"], (int, float))])
+            if self.kwargs["ignore_bands"] is not None:
+                for band in self.kwargs["ignore_bands"]:
+                    # ensure this band exists
+                    assertions.extend([band in json.loads(config.get("Other", "ALL_BANDS"))])
+            passed = all(assertions)
+        except:
+            passed = False
+        return passed
         
     def _selection_criteria(
         self: Self,
@@ -951,7 +1034,11 @@ class Bluewards_Lya_Non_Detect_Selector(Redshift_Selector):
     ) -> bool:
         # extract first Lya non-detect band
         first_Lya_non_detect_band = gal.aper_phot[self.aper_diam]. \
-            SED_results[self.SED_fit_label].phot_rest.first_Lya_non_detect_band
+            SED_results[self.SED_fit_label].phot_rest. \
+                get_first_bluewards_band(
+                    wav_lyman_lim * u.AA,
+                    self.kwargs["ignore_bands"],
+                )
         # if no bands bluewards of Lyman alpha, 
         # select the galaxy by default
         if first_Lya_non_detect_band is None: