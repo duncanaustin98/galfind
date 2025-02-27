1. Fixed issue with photutil moving imports 2. Updating bandpass average flux calculation for photon-counting detectors 3. Fixed issue in __str__ of photometry where Mock_Photometry objects don't have an instrument associated but it is called 4. Remove forcing of filternames to uppercase - may have side effects! 5. Added compression to .h5 in Depth making and EAZY to reduce file size. 6. Added example notebook showing generation of Jaguar photometry.

galfind/EAZY.py
@@ -492,7 +492,7 @@ class EAZY(SED_code):
         fit_pz = 10 ** (fit.lnp)
         fit_zgrid = fit.zgrid
         hf = h5py.File(zPDF_path, "w")
-        hf.create_dataset("z", data=np.array(fit.zgrid))
+        hf.create_dataset("z", data=np.array(fit.zgrid), compression="gzip")
         pz_arr = np.array(
             [
                 np.array(
@@ -508,7 +508,7 @@ class EAZY(SED_code):
                 )
             ]
         )
-        hf.create_dataset("p_z_arr", data=pz_arr)
+        hf.create_dataset("p_z_arr", data=pz_arr, compression="gzip")
         hf.close()
 
     @staticmethod
@@ -516,7 +516,7 @@ class EAZY(SED_code):
         hf = h5py.File(SED_path, "w")
         hf.create_dataset("wav_unit", data=str(wav_unit))
         hf.create_dataset("flux_unit", data=str(flux_unit))
-        hf.create_dataset("z_arr", data=z_arr)
+        hf.create_dataset("z_arr", data=z_arr, compression="gzip")
         # Load best-fitting SEDs
         fit_data_arr = [
             fit.show_fit(
@@ -549,7 +549,7 @@ class EAZY(SED_code):
                 total=len(fit_data_arr),
             )
         ]
-        hf.create_dataset("wav_flux_arr", data=wav_flux_arr)
+        hf.create_dataset("wav_flux_arr", data=wav_flux_arr, compression="gzip")
         hf.close()
 
     def make_fits_from_out(self, out_path: str) -> NoReturn: